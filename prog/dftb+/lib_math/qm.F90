b1d501e614e86951d07cf77cb911054304513dea
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2017-07-30 04:46:34 +0900

    Converted comments and added unitarity ASSERT test

--- prog/dftb+/lib_math/qm.F90
+++ prog/dftb+/lib_math/qm.F90
@@ -7,35 +7,40 @@
 
 #:include 'common.fypp'
 
-!!* contains some miscellaneous QM related bits and pieces.
+!> contains some miscellaneous quantum mechanics related bits and pieces.
 module qm
   use assert
-  use accuracy, only : dp
+  use accuracy, only : dp, rsp
 
   implicit none
 
   public
 
-  !!* constructs a commutator
+  !> constructs a commutator
   interface commutator
     module procedure C_
   end interface
 
-  ! perform a unitary transformation on a matrix $X^\prime = U X U^\dag$
+  ! perform a unitary transformation on a matrix \(X^\prime = U X U^\dag\)
   interface unitary
     module procedure U_cmplx
     module procedure U_real
   end interface
 
+  interface isunitary
+    module procedure isunitary_real
+    module procedure isunitary_cmplx
+  end interface isunitary
+  
 contains
 
-  !!* constructs a commutator for given matrices C = [A,B]
-  !!* @param C result of commutator
-  !!* @param first matrix
-  !!* @param second matrix
+  !> constructs a commutator for given matrices C = [A,B]
   subroutine C_(C,A,B)
+    !> result of commutator
     complex(dp), intent(out) :: C(:,:)
+    !> first matrix  
     complex(dp), intent(in)  :: A(:,:)
+    !> second matrix
     complex(dp), intent(in)  :: B(:,:)
 
     @:ASSERT(all(shape(C)==shape(A)))
@@ -46,12 +51,11 @@ contains
 
   end subroutine C_
 
-  !> unitary transformation on a matrix $X^\prime = U X U^\dag$
-  !! \param X matrix in original basis, U X U* on return.
-  !! \param U unitary matrix
-  !! \todo test that U is actually unitary in an assert_env block
+  !> unitary transformation on a matrix \(X^\prime = U X U^\dag\)
   subroutine U_cmplx(xx, uu)
+    !! matrix in original basis, U X U* on return.  
     complex(dp), intent(inout) :: xx(:,:)
+    !! unitary matrix
     complex(dp), intent(in) :: uu(:,:)
 
     complex(dp) :: work(size(xx,dim=1),size(xx,dim=2))
@@ -59,27 +63,94 @@ contains
     @:ASSERT(all(shape(xx) == shape(uu)))
     @:ASSERT(size(xx, dim=1) == size(xx, dim=2))
 
+    @:ASSERT(isunitary(uu,epsilon(1.0_rsp)))
+    
     work = matmul(xx, transpose(conjg(uu)))
     xx = matmul(uu, work)
 
   end subroutine U_cmplx
-
-  !> unitary transformation on a matrix $X^\prime = U X U^T$
-  !! \param X matrix in original basis, U X U^T on return.
-  !! \param U unitary matrix
-  !! \todo test that U is actually unitary in an assert_env block
+  
+  !> unitary transformation on a matrix \(X^\prime = U X U^\dag\)
   subroutine U_real(xx, uu)
+    !> \param X matrix in original basis, U X U^T on return.
     real(dp), intent(inout) :: xx(:,:)
+    !> unitary matrix    
     real(dp), intent(in) :: uu(:,:)
 
     real(dp) :: work(size(xx,dim=1),size(xx,dim=2))
-
+    
     @:ASSERT(all(shape(xx) == shape(uu)))
     @:ASSERT(size(xx, dim=1) == size(xx, dim=2))
-
+    
+    @:ASSERT(isunitary(uu,epsilon(1.0_rsp)))
+    
     work = matmul(xx, transpose(uu))
     xx = matmul(uu, work)
 
   end subroutine U_real
+
+  !> tests if a matrix is unitary to single precision
+  function isunitary_real(U,tol) result (unitary)
+    !> matrix to test
+    real(dp), intent(in) :: U(:,:)
+    !> tollerance for test
+    real(dp), intent(in) :: tol
+    !> test result
+    logical :: unitary
+    
+    integer :: ii
+    real(dp) :: work(size(U,dim=1),size(U,dim=2))
+
+    @:ASSERT(size(U,dim=1) == size(U,dim=1))
+    @:ASSERT(size(U,dim=1)==size(U,dim=1))
+    
+    work = matmul(U,transpose(U))
+
+    unitary = .true.
+    do ii = 1, size(work,dim=1)
+      if ( abs(work(ii,ii) -1.0_dp) > tol) then
+        unitary = .false.
+        return
+      end if
+      work(ii,ii) = 0.0_dp
+    end do
+    if (any(abs(work) > tol )) then
+      unitary = .false.
+      return
+    end if
+    
+  end function isunitary_real
+
+  !> tests if a matrix is unitary to single precision
+  function isunitary_cmplx(U,tol) result (unitary)
+    !> matrix to test
+    complex(dp), intent(in) :: U(:,:)
+    !> tollerance for test
+    real(dp), intent(in) :: tol
+    !> test result
+    logical :: unitary
+    
+    integer :: ii
+    complex(dp) :: work(size(U,dim=1),size(U,dim=2))
+
+    @:ASSERT(tol >= 0.0_dp)
+    @:ASSERT(size(U,dim=1)==size(U,dim=1))
+
+    work = matmul(U,transpose(conjg(U)))
+
+    unitary = .true.
+    do ii = 1, size(work,dim=1)
+      if ( abs(work(ii,ii) -1.0_dp) > tol) then
+        unitary = .false.
+        return
+      end if
+      work(ii,ii) = 0.0_dp
+    end do
+    if (any(abs(work) > tol )) then
+      unitary = .false.
+      return
+    end if
+    
+  end function isunitary_cmplx
 
 end module qm
