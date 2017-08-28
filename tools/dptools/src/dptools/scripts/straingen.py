63db0adefce3bba77cd2d11c53744f8b5ae0bb5c
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2017-08-28 10:41:46 +0100

    With 10/10 pylint score and moved to a bin/script split

---
+++ tools/dptools/src/dptools/scripts/straingen.py
@@ -0,0 +1,112 @@
+#------------------------------------------------------------------------------#
+#  DFTB+: general package for performing fast atomistic simulations            #
+#  Copyright (C) 2017  DFTB+ developers group                                  #
+#                                                                              #
+#  See the LICENSE file for terms of usage and distribution.                   #
+#------------------------------------------------------------------------------#
+#
+
+'''Applies strain to a DFTB+ gen file.'''
+
+import sys
+import optparse
+import numpy as np
+from dptools.gen import Gen
+from dptools.scripts.common import ScriptError
+
+USAGE = """usage: %prog [options] INPUT N
+
+Strains the geometry found in INPUT by N percent, writing the
+resulting geometries to standard output.
+"""
+
+# Voight convention for 1 index to 2 index for strain tensors
+VOIGHT = [[0, 0], [1, 1], [2, 2], [1, 2], [0, 2], [0, 1]]
+# Labels for the types of strain
+LABELS = {'xx': (0, ), 'yy': (1, ), 'zz': (2, ), 'yz': (3, ), 'xz': (4, ),
+          'xy': (5, ), 'i': (0, 1, 2)}
+
+
+def main(cmdlineargs=None):
+    '''Main driver for straingen.
+
+    Args:
+        cmdlineargs: List of command line arguments. When None, arguments in
+            sys.argv are parsed. (Default: None)
+    '''
+    infile, percentage, options = parse_cmdline_args(cmdlineargs)
+    straingen(infile, percentage, options)
+
+def parse_cmdline_args(cmdlineargs=None):
+    '''Parses command line arguments.
+
+    Args:
+        cmdlineargs: List of command line arguments. When None, arguments in
+            sys.argv are parsed. (Default: None)
+    '''
+    parser = optparse.OptionParser(usage=USAGE)
+    parser.add_option("-o", "--output", action="store", dest="output",
+                      help="override the name of the output file (use '-' for "
+                      "standard out")
+    parser.add_option("-c", "--component", action="store", dest="component",
+                      type=str, default='I', help="strain type to apply"
+                      "(default: I)")
+
+    options, args = parser.parse_args(cmdlineargs)
+
+    if options.component.lower() not in LABELS:
+        msg = "Invalid strain component '" + options.component + "'"
+        raise ScriptError(msg)
+
+    if len(args) != 2:
+        parser.error("You must specify exactly two arguments (input file \
+        then strain amount).")
+    infile = args[0]
+
+    try:
+        percentage = float(args[1])
+    except ValueError:
+        msg = "Invalid scaling '" + args[1] + "'"
+        raise ScriptError(msg)
+    return infile, percentage, options
+
+def straingen(infile, percent, options):
+    '''Strains a geometry from a gen file.
+
+    Args:
+        infile: File containing the gen-formatted geometry
+        percent: Percentage strain of geometry.
+        options: Options (e.g. as returned by the command line parser)
+    '''
+
+    gen = Gen.fromfile(infile)
+    geometry = gen.geometry
+
+    strain = np.zeros((3, 3), dtype=float)
+    for jj in range(3):
+        strain[jj][jj] = 1.0
+
+    components = LABELS[options.component.lower()]
+
+    for ii in components:
+        strain[VOIGHT[ii][0]][VOIGHT[ii][1]] += 0.005*percent
+        strain[VOIGHT[ii][1]][VOIGHT[ii][0]] += 0.005*percent
+
+    if geometry.latvecs is not None:
+        geometry.latvecs = np.dot(geometry.latvecs, strain)
+
+    geometry.coords = np.dot(geometry.coords, strain)
+
+    if options.output:
+        if options.output == "-":
+            outfile = sys.stdout
+        else:
+            outfile = options.output
+    else:
+        if infile.endswith(".gen"):
+            outfile = infile
+        else:
+            outfile = infile + ".gen"
+
+    gen = Gen(geometry, fractional=gen.fractional)
+    gen.tofile(outfile)
