76efa2c7d06fde939929c8d368eb9d9671b4c650
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2017-11-10 00:34:57 +0000

    Change to print kpoints in units of 2pi when relevant

--- prog/dftb+/prg_dftb/initprogram.F90
+++ prog/dftb+/prg_dftb/initprogram.F90
@@ -2271,6 +2271,17 @@ contains
         write(stdout, "(A,T28,I6,':',3F10.6,3X,F10.6)") trim(strTmp), ii, &
             & (kPoint(jj, ii), jj=1, 3), kWeight(ii)
       end do
+      write(stdout,*)
+      do ii = 1, nKPoint
+        if (ii == 1) then
+          write(strTmp, "(A,':')") "K-points in absolute space"
+        else
+          write(strTmp, "(A)") ""
+        end if
+        write(stdout, "(A,T28,I6,':',3F10.6)") trim(strTmp), ii,&
+            & 2.0_dp*pi*matmul(invLatVec,kPoint(:,ii))
+      end do
+      write(stdout, *)
     end if
 
     if (tDispersion) then
--- prog/dftb+/prg_dftb/mainio.F90
+++ prog/dftb+/prg_dftb/mainio.F90
@@ -1157,7 +1157,8 @@ contains
       & tMD, tDerivs, tCoordOpt, tLatOpt, iLatGeoStep, iSccIter, energy, diffElec, sccErrorQ,&
       & indMovedAtom, coord0Out, q0, qInput, qOutput, eigen, filling, orb, species,&
       & tDFTBU, tImHam, tPrintMulliken, orbitalL, qBlockOut, Ef, Eband, TS, E0, pressure, cellVol,&
-      & tAtomicEnergy, tDispersion, tEField, tPeriodic, nSpin, tSpinOrbit, tScc)
+      & tAtomicEnergy, tDispersion, tEField, tPeriodic, nSpin, tSpinOrbit, tScc,&
+      & invLatVec, kPoints)
 
     !> File  ID
     integer, intent(in) :: fd
@@ -1285,6 +1286,12 @@ contains
     !> Is this a self consistent charge calculation
     logical, intent(in) :: tScc
 
+    !> Reciprocal lattice vectors if periodic
+    real(dp), intent(in) :: invLatVec(:,:)
+
+    !> K-points if periodic
+    real(dp), intent(in) :: kPoints(:,:)
+
     real(dp), allocatable :: qInputUpDown(:,:,:), qOutputUpDown(:,:,:), qBlockOutUpDown(:,:,:,:)
     real(dp) :: angularMomentum(3)
     integer :: ang
@@ -1294,6 +1301,7 @@ contains
 
     character(*), parameter :: formatEigen = "(F14.8)"
     character(*), parameter :: formatFilling = "(F12.5)"
+    character(lc) :: strTmp
 
     nAtom = size(q0, dim=2)
     nLevel = size(eigen, dim=1)
@@ -1353,6 +1361,19 @@ contains
           & "     SCC error    "
       write(fd, "(I5, E18.8, E18.8, E18.8, E18.8)") iSCCIter, energy%Eelec, diffElec, sccErrorQ
       write(fd, "(A)") repeat("*", 80)
+      write(fd, *)
+    end if
+
+    if (tPeriodic .and. tLatOpt) then
+      do iK = 1, nKPoint
+        if (iK == 1) then
+          write(strTmp, "(A,':')") "K-points in absolute space"
+        else
+          write(strTmp, "(A)") ""
+        end if
+        write(fd, "(A,T28,I6,':',3F10.6)") trim(strTmp), iK,&
+            & 2.0_dp*pi*matmul(invLatVec,kPoints(:,iK))
+      end do
       write(fd, *)
     end if
 
