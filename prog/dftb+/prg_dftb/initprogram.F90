88deae4d42f81b763c2dc95400bb841e7eb66179
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2017-12-16 23:23:40 +0000

    Mulliken and similar excited properties for multiple states
    Also some changes in formatting of excited state properties.

--- prog/dftb+/prg_dftb/initprogram.F90
+++ prog/dftb+/prg_dftb/initprogram.F90
@@ -1638,9 +1638,6 @@ contains
       end if
 
       if (input%ctrl%lrespini%nstat == 0) then
-        if (input%ctrl%lrespini%tMulliken) then
-          call error("Muliken analysis only available for StateOfInterest non zero.")
-        end if
         if (tForces) then
           call error("Excited forces only available for StateOfInterest non zero.")
         end if
