f576e15bd34432e3ebf9d8ea01251c6061cbedef
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2017-12-29 16:22:07 +0000

    Routines for matrix determinants via LU decomposition

---
+++ prog/dftb+/lib_math/determinant.F90
@@ -0,0 +1,83 @@
+!--------------------------------------------------------------------------------------------------!
+!  DFTB+: general package for performing fast atomistic simulations                                !
+!  Copyright (C) 2017  DFTB+ developers group                                                      !
+!                                                                                                  !
+!  See the LICENSE file for terms of usage and distribution.                                       !
+!--------------------------------------------------------------------------------------------------!
+
+#:include 'common.fypp'
+
+!> Contains routines to calculate matrix determinants
+module determinant
+  use accuracy
+  use lapackroutines
+  implicit none
+
+  private
+
+  interface det
+    module procedure det_real
+    module procedure det_cmplx
+  end interface det
+
+  public :: det
+
+contains
+
+  !> Determinant of a real matrix, matrix destroyed in process
+  function det_real(A) result(det)
+
+    !> The matrix
+    real(dp), intent(inout) :: A(:,:)
+
+    !> resulting determinant
+    real(dp) :: det
+
+    integer, allocatable  :: ipiv(:)
+    integer :: ii, n
+
+    n = minval(shape(A))
+    allocate(ipiv(n))
+
+    call getrf(A,ipiv)
+
+    det = 1.0_dp
+    do ii = 1, n
+      if (ipiv(ii).ne.ii) then
+        det = -det * A(ii,ii)
+      else
+        det = det * A(ii,ii)
+      end if
+    end do
+
+  end function det_real
+
+  !> Determinant of a complex matrix, matrix destroyed in process
+  function det_cmplx(A) result(det)
+
+    !> The matrix
+    complex(dp), intent(inout) :: A(:,:)
+
+    !> resulting determinant
+    complex(dp) :: det
+
+    integer, allocatable  :: ipiv(:)
+    integer :: ii, n
+
+    n = minval(shape(A))
+    allocate(ipiv(n))
+
+    call getrf(A,ipiv)
+
+    det = 1.0_dp
+    do ii = 1, n
+      if (ipiv(ii).ne.ii) then
+        det = -det * A(ii,ii)
+      else
+        det = det * A(ii,ii)
+      end if
+    end do
+
+  end function det_cmplx
+
+end module determinant
