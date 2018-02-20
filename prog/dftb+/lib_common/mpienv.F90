5d46ece04c1daa270e0742526cc1a194e9010e68
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2018-02-20 21:32:23 +0000

    Start of new replica implementation

--- prog/dftb+/lib_common/mpienv.F90
+++ prog/dftb+/lib_common/mpienv.F90
@@ -37,6 +37,9 @@ module mpienv
     !> Group index of the current process (starts with 0)
     integer :: myGroup
 
+    !> Rank within my group
+    integer :: myGroupRank
+
     !> Global rank of the processes in the given group
     integer, allocatable :: groupMembers(:)
 
@@ -45,6 +48,30 @@ module mpienv
 
     !> Whether current process is the group master
     logical :: tGroupMaster
+
+    !> Number of replicas in the global comm world
+    integer :: nReplicas
+
+    !> Communicator to access processes within a replica
+    type(mpifx_comm) :: intraReplicaComm
+
+    !> Communicator between equivalent processors in replicas
+    type(mpifx_comm) :: interReplicaComm
+
+    !> Size of the group containing this replica
+    integer :: replicaCommSize
+
+    !> Replica group index the current process belongs to (starts with 0)
+    integer :: myReplica
+
+    !> Rank within my replica
+    integer :: myReplicaRank
+
+    !> Global rank of the processes in the given replica group
+    integer, allocatable :: replicaMembers(:)
+
+    !> Whether current process is the replica master
+    logical :: tReplicaMaster
 
   end type TMpiEnv
 
@@ -61,32 +88,88 @@ contains
     integer, intent(in) :: nGroup
 
     character(lc) :: tmpStr
-    integer :: myRank, myGroup
+
+    ! temporarily set to 1
+    integer, parameter :: nReplicas = 1
 
     call this%globalComm%init()
-    this%nGroup = nGroup
-    this%groupSize = this%globalComm%size / this%nGroup
-    if (this%nGroup * this%groupSize /= this%globalComm%size) then
-      write(tmpStr, "(A,I0,A,I0,A)") "Number of groups (", this%nGroup,&
+
+    ! number of replicas (structures) in the system
+    this%nReplicas = nReplicas
+
+    ! number of processors in a replica group
+    this%replicaCommSize = this%globalComm%size / this%nReplicas
+
+    if (this%nReplicas * this%replicaCommSize /= this%globalComm%size) then
+      write(tmpStr, "(A,I0,A,I0,A)") "Number of replicas (", this%nReplicas,&
           & ") not compatible with number of processes (", this%globalComm%size, ")"
       call error(tmpStr)
     end if
 
-    this%myGroup = this%globalComm%rank / this%groupSize
-    myRank = mod(this%globalComm%rank, this%groupSize)
-    call this%globalComm%split(this%myGroup, myRank, this%groupComm)
+    ! the replica group this process belongs to
+    this%myReplica = this%globalComm%rank / this%replicaCommSize
+
+    ! rank within the replica group
+    this%myReplicaRank = mod(this%globalComm%rank, this%replicaCommSize)
+
+    ! communicator within this replica
+    call this%globalComm%split(this%myReplica, this%myReplicaRank, this%intraReplicaComm)
+
+    ! array of global process ids within this replica
+    allocate(this%replicaMembers(this%replicaCommSize))
+    call mpifx_allgather(this%intraReplicaComm, this%globalComm%rank, this%replicaMembers)
+
+    ! communicator to equivalent rank processors in different replicas
+    call this%globalComm%split(this%myReplicaRank, this%myReplica, this%interReplicaComm)
+
+    ! number of groups of processors within a replica
+    this%nGroup = nGroup
+
+    ! number of processors in a group
+    this%groupSize = this%intraReplicaComm%size / this%nGroup
+
+    if (this%nGroup * this%groupSize /= this%intraReplicaComm%size) then
+      if (this%nReplicas > 1) then
+        write(tmpStr, "(A,I0,A,I0,A)") "Number of groups (", this%nGroup,&
+            & ") not compatible with number of processes / replica (", this%intraReplicaComm%size,&
+            & ")"
+      else
+        write(tmpStr, "(A,I0,A,I0,A)") "Number of groups (", this%nGroup,&
+            & ") not compatible with number of processes (", this%intraReplicaComm%size, ")"
+      end if
+      call error(tmpStr)
+    end if
+
+    ! the group this process belongs to within the replica
+    this%myGroup = this%intraReplicaComm%rank / this%groupSize
+
+    ! rank within the group
+    this%myGroupRank = mod(this%intraReplicaComm%rank, this%groupSize)
+
+    ! communicator within the group
+    call this%intraReplicaComm%split(this%myGroup, this%myGroupRank, this%groupComm)
+
+    ! array of global process ids within this group
     allocate(this%groupMembers(this%groupSize))
     call mpifx_allgather(this%groupComm, this%globalComm%rank, this%groupMembers)
+    ! equivalent for ids within the replica
+    ! call mpifx_allgather(this%groupComm, this%intraReplicaComm%rank, this%groupMembers)
 
-    myGroup = myRank
-    myRank = this%myGroup
-    call this%globalComm%split(myGroup, myRank, this%interGroupComm)
+    ! communicator to equivalent rank processors in different groups within the replica
+    call this%intraReplicaComm%split(this%myGroupRank, this%myGroup, this%interGroupComm)
 
     this%tGlobalMaster = this%globalComm%master
+    this%tReplicaMaster = this%intraReplicaComm%master
     this%tGroupMaster = this%groupComm%master
 
     if (this%tGlobalMaster .and. .not. this%tGroupMaster) then
       call error("Internal error: Global master process is not a group master process")
+    end if
+    if (this%tGlobalMaster .and. .not. this%tReplicaMaster) then
+      call error("Internal error: Global master process is not a replica master process")
+    end if
+    if (this%tReplicaMaster .and. .not. this%tGroupMaster) then
+      call error("Internal error: Replica master process is not a group master process")
     end if
 
   end subroutine TMpiEnv_init
