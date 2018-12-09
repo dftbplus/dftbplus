63163577729eb0771b687b2a61bf3bdb5c66f146
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2018-12-09 18:01:50 +0000

    Helper library now building correctly

--- external/magmahelper/functions.cpp
+++ external/magmahelper/functions.cpp
@@ -2,12 +2,13 @@
 
 // count GPUs function
 
+const int MAXGPUS = 117; // 117 is large enough to fit all device IDs
 
 extern "C"  void  MagmaGpuNum_Available(int* max_ngpus)
 {
-    magma_device_t devices[117]; // 117 is large enough to fit all device IDs
+    magma_device_t devices[MAXGPUS];
     magma_int_t number_of_gpus;
-    magma_getdevices( devices, 117, &number_of_gpus);
+    magma_getdevices( devices, MAXGPUS, &number_of_gpus);
     *max_ngpus=number_of_gpus;
     return;
 }
