d8358a10554fcd4a578cd921b4b94a08cfda2a04
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2019-07-23 22:11:53 +0100

    Generalised out the wrapped intrinsic with Fypp

--- prog/dftb+/lib_type/wrapped_intrinsics.F90
+++ prog/dftb+/lib_type/wrapped_intrinsics.F90
@@ -11,21 +11,25 @@ module dftbp_wrappedintr
   implicit none
   private
 
-  public :: WrappedInt1, WrappedReal1, WrappedLogical1
+#:set FLAVOURS = [('integer', 'Int', ''), ('real', 'Real', '(dp)'), ('logical','Logical','')]
 
-  !> 1 dimensional integers
-  type :: WrappedInt1
-    integer, allocatable :: data(:)
-  end type WrappedInt1
+#:for _, SUFFIX, _ in FLAVOURS
+#:for DIM in [('1'), ('2')]
 
-  !> 1 dimensional reals
-  type :: WrappedReal1
-    real(dp), allocatable :: data(:)
-  end type WrappedReal1
+  public :: wrapped${SUFFIX}$${DIM}$
 
-  !> 1 dimensional logicals
-  type :: WrappedLogical1
-    logical, allocatable :: data(:)
-  end type WrappedLogical1
+#:endfor
+#:endfor
+
+#:for TYPE, NAME, PREC in FLAVOURS
+#:for DIM, ARRAY in [('1',':'), ('2', ':,:')]
+
+  !> ${DIM}$ dimensional ${TYPE}$
+  type :: wrapped${NAME}$${DIM}$
+    ${TYPE}$${PREC}$, allocatable :: data(${ARRAY}$)
+  end type wrapped${NAME}$${DIM}$
+
+#:endfor
+#:endfor
 
 end module dftbp_wrappedintr
