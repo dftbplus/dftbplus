31f99f17183ac8f4f4a75be9b8a7cd1294b5239f
Franco Bonafé <fbonafe@unc.edu.ar>
2019-11-24 23:39:41 +0100

    Change input of kick direction and update manual and tests

--- prog/dftb+/lib_dftbplus/parser.F90
+++ prog/dftb+/lib_dftbplus/parser.F90
@@ -3938,7 +3938,21 @@ contains
 
     case ("kick")
        input%pertType = iKick
-       call getChildValue(value1, "PolarizationDirection", input%polDir)
+       call getChildValue(value1, "PolarizationDirection", buffer2)
+       select case(unquote(char(buffer2)))
+       case ("x", "X")
+         input%polDir = 1
+       case ("y", "Y")
+         input%polDir = 2
+       case ("z", "Z")
+         input%polDir = 3
+       case ("all", "All", "ALL")
+         input%polDir = 4
+       case default
+         call detailedError(value1, "Wrong specified polarization direction " // char(buffer2) &
+             & // ". Must be x, y, z or all.")
+       end select
+
        if (input%polDir < 1 .or. input%polDir > 4) then
           call detailedError(child, "Wrong specified polarization direction")
        end if
