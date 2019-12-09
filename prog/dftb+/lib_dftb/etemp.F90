c902d5cd5dff38143c3eb5db93c3c1662b67e1c4
kowalct2 <kowalct2@wwu.edu>
2019-12-08 21:42:11 -0800

    Completed my review of initial DeltaDFTB commits

--- prog/dftb+/lib_dftb/etemp.F90
+++ prog/dftb+/lib_dftb/etemp.F90
@@ -93,10 +93,10 @@ contains
     !> Is this a non-Aufbau calculation?
     logical, intent(in) :: tNonAufbau
 
-    !> Is this a spin purified calculation?
+    !> If non-Aufbau, is this a spin purified calculation?
     logical, intent(in) :: tSpinPurify
 
-    !> Which TI-DFTB state is being calculated? 1 = triplet, 2 = mixed
+    !> Which TI-DFTB determinant is being calculated? 1 = triplet, 2 = mixed
     integer, intent(in) :: iDet
 
     !> Number of electrons and spin channel index
