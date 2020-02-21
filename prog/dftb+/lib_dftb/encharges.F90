cc7b80fb892d4053269fdd32b65e6ab1996ad79b
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2020-02-21 12:06:02 +0100

    replace atomic number in DFT-D4 with species array

--- prog/dftb+/lib_dftb/encharges.F90
+++ prog/dftb+/lib_dftb/encharges.F90
@@ -15,8 +15,8 @@
 module dftbp_encharges
   use dftbp_assert
   use dftbp_accuracy
-  use dftbp_constants, only: pi
-  use dftbp_errorfunction, only: erfwrap
+  use dftbp_constants, only : pi
+  use dftbp_errorfunction, only : erfwrap
   use dftbp_coulomb, only : ewaldReal, ewaldReciprocal, derivStressEwaldRec
   use dftbp_blasroutines, only : hemv, gemv, gemm
   use dftbp_lapackroutines, only : symmatinv
