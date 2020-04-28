35da51307423849ba2f25bc33a7f12385de8afa9
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2020-04-28 12:18:46 +0200

    Fix spacing

--- utils/test/travis-ci.sh
+++ utils/test/travis-ci.sh
@@ -12,7 +12,7 @@ cmake_options=(
 mkdir -p _build
 pushd _build
 
-if ["$TRAVIS_PULL_REQUEST" != "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
    cmake -DCMAKE_BUILD_TYPE=Debug "${cmake_options[@]}" ..
 else
    cmake -DCMAKE_BUILD_TYPE=Release "${cmake_options[@]}" ..
