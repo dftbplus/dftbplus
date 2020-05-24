b880f2d0ea18767e346ac326dd892cdd43e1109e
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2020-05-24 22:42:23 +0100

    Re-enable coherence in debug mode
    PR 500 fixed relevant DEBUG behaviour.

--- prog/dftb+/lib_dftbplus/initprogram.F90
+++ prog/dftb+/lib_dftbplus/initprogram.F90
@@ -1523,10 +1523,9 @@ contains
     @:ASSERT(all(shape(species0) == shape(input%geom%species)))
     species0(:) = input%geom%species(:)
 
-    #! temporary comment out until macro is fixed
-    #!#:call DEBUG_CODE
+    #:call DEBUG_CODE
     call inputCoherenceCheck(env, nAtom, coord0, speciesName, species0, tSccCalc)
-    #!#:endcall DEBUG_CODE
+    #:endcall DEBUG_CODE
 
     if (input%ctrl%tHalogenX) then
       if (.not. (t3rd .or. t3rdFull)) then
@@ -3473,8 +3472,7 @@ contains
 
     integer :: iSp
 
-    ! temporary comment out until MPI api is included
-    !if (env%tAPICalculation) then
+    if (env%tAPICalculation) then
 
       if (.not. exactCoherence(env, nAtom)) then
         call error("Coherence failure in number of atoms across nodes")
@@ -3494,7 +3492,7 @@ contains
         call error("Coherence failure in type of calculation : SCC")
       end if
 
-    !end if
+    end if
 
   end subroutine inputCoherenceCheck
 
