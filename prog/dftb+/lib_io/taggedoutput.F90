8c7681868a69a9fdf8e650f8cc88cbb3e389c840
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2020-05-28 14:11:13 +0100

    Added autotest for bond order

--- prog/dftb+/lib_io/taggedoutput.F90
+++ prog/dftb+/lib_io/taggedoutput.F90
@@ -114,6 +114,9 @@ module dftbp_taggedoutput
     !> final molecular orbitals occupations after real-time (Ehrenfest) propagation
     character(lenLabel) :: tdprojocc = 'final_td_proj_occupations'
 
+    !> Sum of bond order values
+    character(lenLabel) :: sumBondOrder = 'sum_bond_orders'
+
     !> total energy including electron TS contribution
     character(lenLabel) :: freeEgy = 'mermin_energy'
 
