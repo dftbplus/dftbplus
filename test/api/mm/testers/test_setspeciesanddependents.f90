d0e8ec16aa95e5690330c81f5b7ec511018e2a39
Alexander Buccheri <alexanderbuccheri@googlemail.com>
2020-06-11 18:36:14 +0100

    resetspecies test has moved to resetspecies_serial, resetspecies is nulk NaCl with ~ 500 atoms, and test_setspeciesanddependents.f90 has been modified such that it only reads in on master then bcasts to all others. NEED to preprocess test_setspeciesanddependents.f90 such that it compiles and runs in serial

--- test/api/mm/testers/test_setspeciesanddependents.f90
+++ test/api/mm/testers/test_setspeciesanddependents.f90
@@ -6,11 +6,13 @@
 ! be used to reset quantities that depend on the global ordering of
 ! atoms species. I.e.
 !
-! species(1:nAtoms) = (/1,2,2,1,...,1/) -> (/1,2,2,1,...,2/),
-! following an MD step. A situation that arises in molecular dynamics
-! packages that uses domain decomposition as a parallelisation scheme.
+!  species(1:nAtoms) = (/1,2,2,1,...,1/) -> (/1,2,2,1,...,2/),
 !
-! All processes read in geometry and all processes contain all atoms.
+! following an MD step. A situation that arises in molecular dynamics
+! packages that use domain decomposition as a parallelisation scheme.
+!
+! Master process reads in geometry and broadcasts to all other processes.
+! Geometry is not distributed upon entry to or in DFTB+.
 ! nAtoms conserved.
 !
 ! Forces are not used to update atomic positions, rather two sets
@@ -68,13 +70,12 @@ program test_setSpeciesAndDependents
   integer, parameter :: requiredThreading = MPI_THREAD_FUNNELED
   integer            :: providedThreading, rank, np, ierr
   integer, parameter :: master_id = 0
-  integer            :: comm
   logical            :: IO
   
   !Local
-  integer, parameter :: nAtoms = 8
   integer, parameter :: Nsteps = 2
-
+  integer            :: nAtoms 
+  
   type(MDstatus_type):: MDstatus
   type(TGeometry)    :: geo
   integer            :: imd
@@ -85,11 +86,11 @@ program test_setSpeciesAndDependents
 
   !Initialise MPI environment
   call mpi_init_thread(requiredThreading, providedThreading, ierr)
-  call mpi_comm_dup(MPI_COMM_WORLD, comm, ierr)
-  call mpi_comm_rank(comm, rank, ierr)
-  call mpi_comm_size(comm, np, ierr)
+  call mpi_comm_rank(MPI_COMM_WORLD, rank, ierr)
+  call mpi_comm_size(MPI_COMM_WORLD, np, ierr)
   IO = (rank == master_id)
-  
+
+  nAtoms = get_number_of_atoms('structure_1.gen')
   MDstatus = MDstatus_type(1, Nsteps)
   allocate(gradients(3,nAtoms))
   allocate(grossCharges(nAtoms))
@@ -105,11 +106,17 @@ program test_setSpeciesAndDependents
     write(imd_lab, '(I2)') imd
     fname = 'structure_'//trim(adjustl(imd_lab))//'.gen'
     call read_in_geo(trim(adjustl(fname)), geo)
+    call broadcast_geometry(MPI_COMM_WORLD, geo)
+    
+    if (geo%nAtom /= nAtoms) then
+       write(*,*) 'Erro: Number of atoms not conserved between MD steps'
+       call mpi_abort(MPI_COMM_WORLD, 0, ierr)
+    endif
 
     if(imd == MDstatus%initial_step) then
-      Call TDftbPlus_init(dftb, output_unit, comm)
+      Call TDftbPlus_init(dftb, output_unit, MPI_COMM_WORLD)
       Call initialise_dftbplus_tree(geo, dftb, hsd_tree)
-      !Dump hsd tree to fort.1
+      !Dump hsd tree to fort.1 if debugging 
       !Call dumpHsd(hsd_tree%hsdTree, 001)
       Call dftb%setupCalculator(hsd_tree)
     endif
@@ -145,7 +152,7 @@ program test_setSpeciesAndDependents
 
   ! Write file for internal test system, using the last structure that was run
   call writeAutotestTag(merminEnergy=merminEnergy, gradients=gradients, stressTensor=stressTensor,&
-      & grossCharges=grossCharges)
+       & grossCharges=grossCharges)
 
   call mpi_finalize(ierr)
 
@@ -247,11 +254,40 @@ contains
   end function count_substrings
 
 
-  !> Read DFTB+ geometry file
+  !> Get number of atoms from DFTB+ geometry file
+  function get_number_of_atoms(fname, header) result(nAtoms)
+    implicit none
+
+    character(len=*), intent(in)  :: fname
+    integer, optional, intent(in) :: header
+    integer :: nAtoms
+
+    character(len=1) :: boundary
+    integer :: io_unit , i
+
+    if(IO) then
+       open(newunit=io_unit,file=fname)
+       
+       if(present(header))then
+          do i=1,header
+             read(io_unit,*)
+          enddo
+       endif
+       
+       read(io_unit,*) nAtoms, boundary
+       
+       close(io_unit)
+    endif
+    call mpi_bcast(nAtoms, 1, MPI_INTEGER, master_id, MPI_COMM_WORLD, ierr)
+
+  end function get_number_of_atoms
+  
+
+  !> Read DFTB+ geometry file onto master_id
   subroutine read_in_geo(fname, geo, header)
     implicit none
 
-    character(len=*), intent(in)    :: fname
+    character(len=*), intent(in)  :: fname
     type(TGeometry),  intent(out) :: geo
     integer, optional, intent(in) :: header
         
@@ -261,75 +297,121 @@ contains
     character(len=100) :: line2
     integer :: io_unit 
 
-    !Initialise geo components
-    geo%nAtom = 0
-    geo%nSpecies = 0
-
-    ! Open file
-    open(newunit=io_unit,file=fname)
-
-    !Skip nlines of header
-    if(present(header))then
-       do i=1,header
-          read(io_unit,*)
+    if(IO) then
+       !Initialise geo components
+       geo%nAtom = 0
+       geo%nSpecies = 0
+       
+       ! Open file
+       open(newunit=io_unit,file=fname)
+       
+       !Skip nlines of header
+       if(present(header))then
+          do i=1,header
+             read(io_unit,*)
+          enddo
+       endif
+       
+       !Assign geo data from file
+       !Line 1
+       read(io_unit,*) geo%nAtom, boundary
+       
+       if(boundary=='S')then
+          geo%tPeriodic = .true.
+          geo%tFracCoord = .false.
+       elseif(boundary=='F')then
+          geo%tPeriodic  = .true.
+          geo%tFracCoord = .true.
+       elseif(boundary=='C')then
+          geo%tPeriodic = .false.
+          geo%tFracCoord = .false.
+       endif
+       
+       !Line 2.
+       read(io_unit, '(A)') line2
+       Nspecies = count_substrings(line2)
+       backspace io_unit
+       
+       allocate(species_list(Nspecies))
+       species_list=' '
+       read(io_unit,*) species_list(:)
+       
+       geo%nSpecies = Nspecies
+       if(.not. allocated(geo%speciesNames)) allocate(geo%speciesNames(geo%nSpecies))
+       geo%speciesNames = ' '
+       do ia=1,geo%nSpecies
+          geo%speciesNames(ia) = species_list(ia)
        enddo
-    endif
-
-    !Assign geo data from file
-    !Line 1
-    read(io_unit,*) geo%nAtom, boundary
-
-    if(boundary=='S')then
-       geo%tPeriodic = .true.
-       geo%tFracCoord = .false.
-    elseif(boundary=='F')then
-       geo%tPeriodic  = .true.
-       geo%tFracCoord = .true.
-    elseif(boundary=='C')then
-       geo%tPeriodic = .false.
-       geo%tFracCoord = .false.
-    endif
-
-    !Line 2.
-    read(io_unit, '(A)') line2
-    Nspecies = count_substrings(line2)
-    backspace io_unit
-
-    allocate(species_list(Nspecies))
-    species_list=' '
-    read(io_unit,*) species_list(:)
-
-    geo%nSpecies = Nspecies
-    if(.not. allocated(geo%speciesNames)) allocate(geo%speciesNames(geo%nSpecies))
-    geo%speciesNames = ' '
-    do ia=1,geo%nSpecies
-      geo%speciesNames(ia) = species_list(ia)
-    enddo
-
-    !Lines 3 -> 3+Natoms
-    if(.not. allocated(geo%species))   allocate(geo%species(geo%nAtom))
-    if(.not. allocated(geo%coords))    allocate(geo%coords(3, geo%nAtom))
-    geo%species = 0
-    geo%coords = 0
-    do ia=1,geo%nAtom
-       read(io_unit,*) i,geo%species(ia),geo%coords(1:3,ia)
-       geo%coords(1:3,ia) = geo%coords(1:3,ia) * AA__Bohr
-    enddo
-
-    if(boundary /= 'C')then
-       !Origin
-       read(io_unit, *)
-
-       !Lattice vectors in rows in .gen BUT as columns internally
-       if(.not. allocated(geo%latVecs)) allocate(geo%latVecs(3,3))
-       do ia=1,3
-          read(io_unit,*) geo%latVecs(:,ia)
+       
+       !Lines 3 -> 3+Natoms
+       if(.not. allocated(geo%species))   allocate(geo%species(geo%nAtom))
+       if(.not. allocated(geo%coords))    allocate(geo%coords(3, geo%nAtom))
+       geo%species = 0
+       geo%coords = 0
+       do ia=1,geo%nAtom
+          read(io_unit,*) i,geo%species(ia),geo%coords(1:3,ia)
+          geo%coords(1:3,ia) = geo%coords(1:3,ia) * AA__Bohr
        enddo
-       geo%latVecs(:,:) =  geo%latVecs(:,:) * AA__Bohr
+       
+       if(boundary /= 'C')then
+          !Origin
+          read(io_unit, *)
+          
+          !Lattice vectors in rows in .gen BUT as columns internally
+          if(.not. allocated(geo%latVecs)) allocate(geo%latVecs(3,3))
+          do ia=1,3
+             read(io_unit,*) geo%latVecs(:,ia)
+          enddo
+          geo%latVecs(:,:) =  geo%latVecs(:,:) * AA__Bohr
+       endif
+       
+       close(io_unit)
     endif
-
-    close(io_unit)
-
+    
   end subroutine read_in_geo
 
+  !> Broadcast geometry from master_id to all processes 
+  subroutine broadcast_geometry(comm, geo)
+
+    !> MPI communicator
+    integer, intent(in) :: comm
+    !> Geometry: Filled on master_id, uninitialised on all other processes
+    type(TGeometry),  intent(inout) :: geo
+
+    integer :: ierr, lenSN, ia
+    
+    call mpi_bcast(geo%nAtom,      1, MPI_INTEGER, master_id, comm, ierr)
+    call mpi_bcast(geo%tPeriodic,  1, MPI_LOGICAL, master_id, comm, ierr)
+    call mpi_bcast(geo%tFracCoord, 1, MPI_LOGICAL, master_id, comm, ierr)
+    call mpi_bcast(geo%nSpecies,   1, MPI_INTEGER, master_id, comm, ierr)
+
+    if(.not. allocated(geo%speciesNames)) then
+       allocate(geo%speciesNames(geo%nSpecies))
+    endif
+
+    lenSN = len(geo%speciesNames)
+    call mpi_bcast(geo%speciesNames, lenSN * geo%nSpecies, MPI_CHARACTER, master_id, comm, ierr)
+
+    if(.not. allocated(geo%species)) then
+       allocate(geo%species(geo%nAtom))
+    endif
+
+    if(.not. allocated(geo%coords)) then
+       allocate(geo%coords(3, geo%nAtom))
+    endif
+
+    call mpi_bcast(geo%species, geo%nAtom, MPI_INTEGER, master_id, comm, ierr)
+    call mpi_bcast(geo%coords, 3 * geo%nAtom, MPI_DOUBLE_PRECISION, master_id, comm, ierr)
+
+    if (geo%tPeriodic) then
+       if(.not. allocated(geo%latVecs)) then
+          allocate(geo%latVecs(3,3))
+       endif
+       call mpi_bcast(geo%latVecs, 9, MPI_DOUBLE_PRECISION, master_id, comm, ierr)
+    endif
+
+    call mpi_barrier(comm, ierr)
+    
+  end subroutine broadcast_geometry
+  
 end program test_setSpeciesAndDependents
