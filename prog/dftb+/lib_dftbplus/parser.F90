bbcb651daaba582e7e8203b52d28350ac0bf1878
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2020-08-11 21:55:17 +0100

    Fix parser bug for net charge write

--- prog/dftb+/lib_dftbplus/parser.F90
+++ prog/dftb+/lib_dftbplus/parser.F90
@@ -4752,7 +4752,7 @@ contains
 
     call getChildValue(node, "MullikenAnalysis", ctrl%tPrintMulliken, .true.)
     if (ctrl%tPrintMulliken) then
-      call getChildValue(child, "WriteNetCharges", ctrl%tNetAtomCharges, default=.false.)
+      call getChildValue(node, "WriteNetCharges", ctrl%tNetAtomCharges, default=.false.)
       call getChild(node, "CM5", child, requested=.false.)
       if (associated(child)) then
         allocate(ctrl%cm5Input)
