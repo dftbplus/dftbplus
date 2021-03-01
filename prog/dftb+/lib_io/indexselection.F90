3baa8b7d90ff0ca06377ec2d558b01da0ea752ea
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2021-03-01 18:27:02 +0100

    Further gfortran7 workaround

--- prog/dftb+/lib_io/indexselection.F90
+++ prog/dftb+/lib_io/indexselection.F90
@@ -14,7 +14,7 @@
 !>
 module dftbp_io_indexselection
   use dftbp_assert
-  use dftbp_accuracy, only : dp
+  use dftbp_accuracy, only : dp, mc
   use dftbp_charmanip, only : tolower
   use dftbp_status, only : TStatus
   implicit none
@@ -84,7 +84,9 @@ module dftbp_io_indexselection
     integer :: indexRange(2)
 
     !> Names of the species, in case it is an atom index selection. Shape: [nSpecies]
-    character(:), allocatable :: speciesNames(:)
+    !> Workaround: gcc7 assignment broken
+    !> character(:), allocatable :: speciesNames(:)
+    character(mc), allocatable :: speciesNames(:)
 
     !> Species of each atom, in case it is an atom index selection. Shape: [endInd - startInd + 1]
     integer, allocatable :: species(:)
