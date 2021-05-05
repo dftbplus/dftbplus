87b00c77272efb44a854bc9749a4cac6f91de130
Bálint Aradi <aradi@uni-bremen.de>
2021-05-05 17:07:20 +0200

    Update year in source header copyrights

--- prog/phonons/libnegf_int.F90
+++ prog/phonons/libnegf_int.F90
@@ -1,3 +1,10 @@
+!--------------------------------------------------------------------------------------------------!
+!  DFTB+: general package for performing fast atomistic simulations                                !
+!  Copyright (C) 2006 - 2021  DFTB+ developers group                                               !
+!                                                                                                  !
+!  See the LICENSE file for terms of usage and distribution.                                       !
+!--------------------------------------------------------------------------------------------------!
+
 ! **************************************************************************
 ! *                INTERFACE of LIBNEGF for DFTB+
 ! *
