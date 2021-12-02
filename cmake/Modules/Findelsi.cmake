be0d9d01e5a1983aa690b2d0e00e298e3a0a8dfe
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-12-02 16:14:03 +0100

    Use CMake variable expansion to inject target in libmbd build

--- cmake/Modules/Findelsi.cmake
+++ cmake/Modules/Findelsi.cmake
@@ -1,29 +1,34 @@
-find_package(PkgConfig QUIET)
-pkg_check_modules(ELSI QUIET elsi)
-if(ELSI_FOUND)
-  message(STATUS "Found 'elsi' via pkg-config")
+if(NOT TARGET elsi::elsi)
+  find_package(PkgConfig QUIET)
+  pkg_check_modules(ELSI QUIET elsi)
+  if(ELSI_FOUND)
+    message(STATUS "Found 'elsi' via pkg-config")
 
-  add_library(elsi::elsi INTERFACE IMPORTED)
-  target_link_libraries(
-    elsi::elsi
-    INTERFACE
-    "${ELSI_LINK_LIBRARIES}"
-  )
-  target_include_directories(
-    elsi::elsi
-    INTERFACE
-    "${ELSI_INCLUDE_DIRS}"
-  )
-
-  if("${ELSI_LINK_LIBRARIES}" MATCHES "pexsi")
-    add_library(elsi::pexsi INTERFACE IMPORTED)
+    add_library(elsi::elsi INTERFACE IMPORTED)
     target_link_libraries(
       elsi::elsi
       INTERFACE
-      "stdc++"
+      "${ELSI_LINK_LIBRARIES}"
     )
-  endif()
+    target_include_directories(
+      elsi::elsi
+      INTERFACE
+      "${ELSI_INCLUDE_DIRS}"
+    )
 
-  # DFTB+ checks for the lowercase variable name
-  set(elsi_VERSION "${ELSI_VERSION}")
+    if("${ELSI_LINK_LIBRARIES}" MATCHES "pexsi")
+      add_library(elsi::pexsi INTERFACE IMPORTED)
+      target_link_libraries(
+        elsi::elsi
+        INTERFACE
+        "stdc++"
+      )
+    endif()
+
+    # libmbd checks for lowercase variable name
+    set(elsi_FOUND ${ELSI_FOUND})
+
+    # DFTB+ checks for the lowercase variable name
+    set(elsi_VERSION "${ELSI_VERSION}")
+  endif()
 endif()
