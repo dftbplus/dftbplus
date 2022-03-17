bfe0fe6e92902b3d69ef704bfb58b809817f2d65
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2022-03-17 13:21:19 +0000

    Work around in API for external potentials
    Flags geometry as having changed if the potentials change (forcing
    re-evaluation of the ground state).
    
    Added test for finite difference in field via pyapi (with commented
    out code for other response properties than dipole moment)

--- src/dftbp/dftbplus/mainapi.F90
+++ src/dftbp/dftbplus/mainapi.F90
@@ -251,6 +251,10 @@ contains
     end if
     main%isExtField = .true.
 
+    ! work around for lack (at the moment) for a flag to re-calculate ground state even if
+    ! geometries are unchanged.
+    main%tCoordsChanged = .true.
+
   end subroutine setExternalPotential
 
 
