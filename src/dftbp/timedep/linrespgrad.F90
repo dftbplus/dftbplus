132197262e766f0674383de92cb2bb2d00adf6fb
thomas-niehaus <33483198+thomas-niehaus@users.noreply.github.com>
2023-03-28 10:13:31 +0200

    Update src/dftbp/timedep/linrespgrad.F90
    Co-authored-by: Bálint Aradi <aradi@uni-bremen.de>

--- src/dftbp/timedep/linrespgrad.F90
+++ src/dftbp/timedep/linrespgrad.F90
@@ -706,11 +706,11 @@ contains
         ! This overwrites T, RHS and W
         numNAC = this%indNACouplings(2) - this%indNACouplings(1) + 1
         numNAC = numNAC * (numNAC-1) / 2
-        ALLOCATE(nacv(size(excgrad,dim=1),size(excgrad,dim=2),numNAC))
-        ALLOCATE(xpyn, mold = xpy(:,1))
-        ALLOCATE(xpym, mold = xpy(:,1))
-        ALLOCATE(xmyn, mold = xpy(:,1))
-        ALLOCATE(xmym, mold = xpy(:,1))
+       allocate(nacv(size(excgrad, dim=1), size(excgrad, dim=2), numNAC))
+        allocate(xpyn, mold=xpy(:,1))
+        allocate(xpym, mold=xpy(:,1))
+        allocate(xmyn, mold=xpy(:,1))
+        allocate(xmym, mold=xpy(:,1))
 
         iNac = 0
         nacv = 0.0_dp
