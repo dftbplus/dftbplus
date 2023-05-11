b43314d47fe2f787dfaedece8409b4c5df51ecfc
Thomas Niehaus <thomas.niehaus@univ-lyon1.fr>
2023-05-11 19:05:35 +0200

    Update src/dftbp/timedep/linrespgrad.F90
    Co-authored-by: Ben Hourahine <bhourahine@gmail.com>

--- src/dftbp/timedep/linrespgrad.F90
+++ src/dftbp/timedep/linrespgrad.F90
@@ -4162,7 +4162,7 @@ contains
         ptmp1 = xpyn(ias) * xpym(ibs) + xmyn(ias) * xmym(ibs)&
               & + xpym(ias) * xpyn(ibs) + xmym(ias) * xmyn(ibs)
    
-        ptmp2 = (xpyn(ias) * xmym(ibs) + xmyn(ias) * xpym(ibs) + &
+        ptmp2 = (xpyn(ias) * xmym(ibs) + xmyn(ias) * xpym(ibs)&
               & + xpym(ias) * xmyn(ibs) + xmym(ias) * xpyn(ibs)) 
 
         tmp3 = xpyn(ias) * xpym(ibs) + xmyn(ias) * xmym(ibs)
