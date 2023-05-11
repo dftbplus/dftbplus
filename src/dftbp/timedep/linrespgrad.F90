939accf44066fe2afcc37360056cfef548694b87
Thomas Niehaus <thomas.niehaus@univ-lyon1.fr>
2023-05-11 19:03:46 +0200

    Update src/dftbp/timedep/linrespgrad.F90
    Co-authored-by: Ben Hourahine <bhourahine@gmail.com>

--- src/dftbp/timedep/linrespgrad.F90
+++ src/dftbp/timedep/linrespgrad.F90
@@ -3968,7 +3968,7 @@ contains
     end do
     
     call getHplusXYfr(sym, nXoo, nXvv, nAtom, getIA, getIJ, getAB, win, iAtomStart,&
-      &  species0, ovrXev, grndEigVecs, frGamma, spinW, transChrg, p, vecHoo=vecHoo)
+      & species0, ovrXev, grndEigVecs, frGamma, spinW, transChrg, p, vecHoo=vecHoo)
 
     do s = 1, nSpin
       do ij = 1, nXoo(s)
