f40476b5e4d94b0aed72bf20050507cd79f3f1f7
Thomas Niehaus <thomas.niehaus@univ-lyon1.fr>
2023-05-11 19:00:35 +0200

    Update src/dftbp/timedep/linrespgrad.F90
    Co-authored-by: Ben Hourahine <bhourahine@gmail.com>

--- src/dftbp/timedep/linrespgrad.F90
+++ src/dftbp/timedep/linrespgrad.F90
@@ -765,7 +765,7 @@ contains
               ! compute + component of RHS for Z-vector eq. in the NaCoupling case
               ! also computes the + components of W and T
               call getNadiaZvectorEqRHS(tRangeSep, xpy(:,nCoupLev), xmy(:,nCoupLev),& 
-                 & xpy(:,mCoupLev), xmy(:,mCoupLev), win, iAtomStart, nocc_ud, transChrg,     &
+                 & xpy(:,mCoupLev), xmy(:,mCoupLev), win, iAtomStart, nocc_ud, transChrg,&
                  & getIA, getIJ, getAB, iatrans, this%nAtom, species0, grndEigVal, ovrXev,    &
                  & grndEigVecs, gammaMat, lrGamma, this%spinW, omegaAvg, sym, rhs, t,         &
                  & wov, woo, wvv)
