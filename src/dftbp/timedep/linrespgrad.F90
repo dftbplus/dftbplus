e979713c0f389f6aa699ed09f5ca66af418ac188
Thomas Niehaus <thomas.niehaus@univ-lyon1.fr>
2023-05-11 18:56:43 +0200

    Update src/dftbp/timedep/linrespgrad.F90
    Co-authored-by: Ben Hourahine <bhourahine@gmail.com>

--- src/dftbp/timedep/linrespgrad.F90
+++ src/dftbp/timedep/linrespgrad.F90
@@ -788,7 +788,7 @@ contains
               end do
 
               call addNadiaGradients(sym, nxov_rd, this%nAtom, species0, iAtomStart, norb, nocc_ud,&
-                & getIA, getIJ, getAB, win, grndEigVecs, pc, ovrXev, dq, dqex, gammaMat, lrGamma,  &
+                & getIA, getIJ, getAB, win, grndEigVecs, pc, ovrXev, dq, dqex, gammaMat, lrGamma,&
                 & this%HubbardU, this%spinW, shift, woo, wov, wvv, transChrg, xpyn, xmyn, xpym,    &
                 & xmym, coord0, orb, skHamCont, skOverCont, derivator, rhoSqr, deltaRho, tRangeSep,&
                 & rangeSep, nacv(:,:,iNac))
