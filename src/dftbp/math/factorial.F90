fd4f4f9ecc6270bb86e0408e24b5ce5da2a7175a
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2023-05-14 13:04:27 +0100

    Fix integer factorial bug
    Was introduced in 4cbab8967 but fortunately the integer case is not
    used anywhere (yet).

--- src/dftbp/math/factorial.F90
+++ src/dftbp/math/factorial.F90
@@ -37,6 +37,7 @@ contains
     @:ASSERT(size(nbang)==n+1)
     nbang(0)=1
     do i=1,n-1
+      nbang(i) = nbang(i-1) * i
       @:ASSERT(nbang(i) <= huge(1) / (i+1) )
     end do
     nbang(n)=nbang(n-1)*n
