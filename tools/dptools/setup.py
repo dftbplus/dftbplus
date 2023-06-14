853ee5160cdeecb7ffff5d009771f72f37903767
Bálint Aradi <aradi@uni-bremen.de>
2023-06-14 16:50:38 +0200

    Bump version number to 23.1

--- tools/dptools/setup.py
+++ tools/dptools/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name="dptools",
-    version='22.2.0',
+    version='23.1',
     description="Tools to process DFTB+ related data",
     author="DFTB+ developers",
     url="http://www.dftbplus.org",
