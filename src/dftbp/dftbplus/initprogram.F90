67a5abad3757e2a96749ad3f084abe9ca991a040
Thomas Niehaus <thomas.niehaus@univ-lyon1.fr>
2023-08-31 09:49:53 +0200

    Update src/dftbp/dftbplus/initprogram.F90
    Co-authored-by: Ben Hourahine <bhourahine@gmail.com>

--- src/dftbp/dftbplus/initprogram.F90
+++ src/dftbp/dftbplus/initprogram.F90
@@ -4799,7 +4799,7 @@ contains
         if(this%linearResponse%isCIopt) then
           if (.not. this%linearResponse%tNaCoupling) then
             call error("Optimization of CI requires StateCouplings keyword.")
-          end if    
+          end if
           dLev = this%linearResponse%indNACouplings(2) - this%linearResponse%indNACouplings(1) + 1 
           if (this%linearResponse%indNACouplings(1) == 0) then
             allocate(this%excitedDerivs(3, this%nAtom, dLev-1))
