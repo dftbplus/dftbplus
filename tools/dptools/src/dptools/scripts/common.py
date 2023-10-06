8c77fd069338e1bf392464c4821a891766ff1e7f
Tammo van der Heide <vanderhe@uni-bremen.de>
2023-10-06 11:06:39 +0200

    Add automatic and manual alignment options to dp_dos

--- tools/dptools/src/dptools/scripts/common.py
+++ tools/dptools/src/dptools/scripts/common.py
@@ -8,6 +8,24 @@
 
 '''Common things needed by command line scripts.'''
 
+import numpy as np
+
 
 class ScriptError(Exception):
     '''Exception thrown by command line scripts.'''
+
+
+def find_auto_alignment(bandout):
+    '''
+    Integer occupations: finds the energy shift required to set the VBM to zero
+    Fractional occupations: finds Fermi-type level
+
+    Args:
+        bandout (BandOut): representation of a band.out like file
+
+    '''
+
+    eigvals = bandout.eigvalarray[:, :, :, 0]
+    occ = bandout.eigvalarray[:, :, :, 1]
+
+    return np.max(eigvals[np.where(occ >= np.max(occ) / 2.0)])
