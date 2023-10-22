26c612be1fdc68c92636365f693710c04c28c5cf
Pavel Stishenko <stishenkop@cardiff.ac.uk>
2023-10-22 23:11:52 +0100

    ASI API bindings
    Added electrostatic potential C API
    
    Added API functions for ASI API implemenation. Including import and
    export of Hamiltonian, Overlap and Density Matrices.
    
    Lock down ASI incompatible options when the interface is active (note,
    manual keyword required for changes in H/S due to limitations of ASI
    at the moment):
    
    * Forces can't be used if the hamiltonian is changed without matrix
    element derivatives.
    
    * SCC can't work unless the density matrix extraction is used to update
    the hamiltonian (no way of knowing at the moment).
    
    * Can't evaluate Mulliken populations if S changes at the moment.
    
    Note: imp_ham tester contains workaround for icx compiler 24.0 problem
    on returning bool.
    
    Co-authored-by: Ben Hourahine <benjamin.hourahine@strath.ac.uk>

--- src/dftbp/dftbplus/inputdata.F90
+++ src/dftbp/dftbplus/inputdata.F90
@@ -144,6 +144,16 @@ module dftbp_dftbplus_inputdata
     !> Choice of electronic hamiltonian
     integer :: hamiltonian = hamiltonianTypes%none
 
+  #:if WITH_API
+    !> Is this ASI callback interface for H,S,P enabled (Stishenko et al.,
+    !! https://doi.org/10.21105/joss.05186)
+    logical :: isAsiCallbackEnabled = .false.
+
+    !> Is the ASI modifying H or S matrices
+    logical :: isAsiChangingTheModel = .false.
+
+  #:endif
+
     !> Random number generator seed
     integer :: iSeed = 0
 
