34a4d65f2541fb55b1033305d641c7f3cb5b142d
Tobias Melson <tobias.melson@mpcdf.mpg.de>
2024-01-04 15:30:08 +0100

    Facilitate vectorization of ewaldReciprocal

--- src/dftbp/dftb/coulomb.F90
+++ src/dftbp/dftb/coulomb.F90
@@ -2050,19 +2050,24 @@ contains
     !> contribution to the sum
     real(dp) :: recSum
 
-    real(dp) :: gg(3), g2
+    real(dp), allocatable :: recSumArray(:)
+
+    real(dp) :: g2Factor, g2, gr
     integer :: iG
 
     @:ASSERT(vol > 0.0_dp)
 
-    recSum = 0.0_dp
+    g2Factor = -1.0_dp / (4.0_dp*alpha**2)
+
+    allocate(recSumArray(size(gVec, dim=2)), source=0.0_dp)
+
     do iG = 1, size(gVec, dim=2)
-      gg = gVec(:,iG)
-      g2 = sum(gg(:)**2)
-      recSum = recSum + exp(-g2/(4.0_dp*alpha**2))/g2 * cos(dot_product(gg,rr))
+      g2 = gVec(1,iG)**2 + gVec(2,iG)**2 + gVec(3,iG)**2
+      gr = gVec(1,iG) * rr(1) + gVec(2,iG) * rr(2) + gVec(3,iG) * rr(3)
+      recSumArray(iG) = exp(g2Factor * g2)/g2 * cos(gr)
     end do
     ! note factor of 2 as only summing half of reciprocal space
-    recSum = 2.0_dp * recSum * 4.0_dp * pi / vol
+    recSum = 2.0_dp * sum(recSumArray) * 4.0_dp * pi / vol
 
   end function ewaldReciprocal
 
