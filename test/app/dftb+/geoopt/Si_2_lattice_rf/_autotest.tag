dftbplus/dftbplus

commit 6e6c6f0841501738964201a25117be99e19b1437

Refactor packaging and OOP structure slightly

src/dftbp/dftbplus/input/geoopt.F90
@@ -1,0 +1,229 @@
+!--------------------------------------------------------------------------------------------------!
+!  DFTB+: general package for performing fast atomistic simulations                                !
+!  Copyright (C) 2006 - 2021  DFTB+ developers group                                               !
+!                                                                                                  !
+!  See the LICENSE file for terms of usage and distribution.                                       !
+!--------------------------------------------------------------------------------------------------!
+
+!> Module to read input from HSD tree
+module dftbp_dftbplus_input_geoopt
+  use dftbp_common_accuracy, only : dp, lc
+  use dftbp_common_globalenv, only : stdOut
+  use dftbp_common_unitconversion, only : timeUnits, lengthUnits, energyUnits, forceUnits
+  use dftbp_extlibs_xmlf90, only : fnode, string, char, getNodeName
+  use dftbp_geoopt_package, only : TFilterInput, TOptimizerInput, TRationalFuncInput,&
+      & TLbfgsInput, TFireInput, TOptTolerance
+  use dftbp_io_charmanip, only : unquote
+  use dftbp_io_hsdutils, only : getChild, getChildValue, setChild, detailedError, &
+      & detailedWarning, getSelectedAtomIndices
+  use dftbp_io_hsdutils2, only : convertByMul
+  use dftbp_type_typegeometry, only : TGeometry
+  implicit none
+
+  private
+  public :: readGeoOptInput, TGeoOptInput
+
+
+  !> General input wrapper for optimizers in this package
+  type :: TGeoOptInput
+
+    !> Input for coordinate transformation and filter step
+    type(TFilterInput) :: filter
+
+    !> Optimizer input choice
+    class(TOptimizerInput), allocatable :: optimizer
+
+    !> Tolerances for optimization
+    type(TOptTolerance) :: tolerance
+
+    !> Number of allowed geometry optimization steps
+    integer :: nGeoSteps = huge(1) - 1
+
+    !> Prefix of the output file name
+    character(len=:), allocatable :: outFile
+
+  end type TGeoOptInput
+
+
+contains
+
+  !> General entry point to read geometry optimization
+  subroutine readGeoOptInput(node, geom, input)
+
+    !> Node to get the information from
+    type(fnode), pointer, intent(in) :: node
+
+    !> Geometry of the system
+    type(TGeometry), intent(in) :: geom
+
+    !> Control structure to be filled
+    type(TGeoOptInput), intent(out) :: input
+
+    type(fnode), pointer :: child, value1
+    type(string) :: buffer
+
+    call getChildValue(node, "Optimizer", child)
+    call readOptimizerInput(child, input%optimizer)
+
+    call getChildValue(node, "Filter", child, "Cartesian")
+    call getNodeName(child, buffer)
+
+    select case (char(buffer))
+    case default
+      call detailedError(node, "Invalid filter name.")
+    case("cartesian")
+      call readFilterInput(child, geom, input%filter)
+    end select
+
+    call getChildValue(node, "Convergence", value1, "", child=child, allowEmptyValue=.true.)
+    call readOptTolerance(child, input%tolerance)
+
+    call getChildValue(node, "MaxSteps", input%nGeoSteps, 20*geom%nAtom)
+
+    call getChildValue(node, "OutputPrefix", buffer, "geo_end")
+    input%outFile = trim(unquote(char(buffer)))
+
+  end subroutine readGeoOptInput
+
+
+  !> Reads the optimizer
+  subroutine readOptimizerInput(node, input)
+
+    !> Optimizer node
+    type(fnode), pointer, intent(in) :: node
+
+    !> Control structure to be filled
+    class(TOptimizerInput), allocatable, intent(out) :: input
+
+    type(TFireInput), allocatable :: fireInput
+    type(TLbfgsInput), allocatable :: lbfgsInput
+    type(TRationalFuncInput), allocatable :: rationalFuncInput
+    type(string) :: buffer
+
+    call getNodeName(node, buffer)
+    select case (char(buffer))
+    case default
+      call detailedError(node, "Invalid optimizer name.")
+    case("fire")
+        allocate(fireInput)
+        call readFireInput(node, fireInput)
+        call move_alloc(fireInput, input)
+    case("lbfgs")
+      allocate(lbfgsInput)
+      call readLbfgsInput(node, lbfgsInput)
+      call move_alloc(lbfgsInput, input)
+    case("rf")
+      allocate(rationalFuncInput)
+      call readRationalFuncInput(node, rationalFuncInput)
+      call move_alloc(rationalFuncInput, input)
+    end select
+
+  end subroutine readOptimizerInput
+
+
+  !> Entry point for reading input for cartesian geometry transformation filter
+  subroutine readFilterInput(node, geom, input)
+
+    !> Node to get the information from
+    type(fnode), pointer, intent(in) :: node
+
+    !> Geometry of the system
+    type(TGeometry), intent(in) :: geom
+
+    !> Control structure to be filled
+    type(TFilterInput), intent(out) :: input
+
+    type(fnode), pointer :: child
+    type(string) :: buffer
+
+    call getChildValue(node, "LatticeOpt", input%lattice, .false.)
+    call getChildValue(node, "MovedAtoms", buffer, "1:-1", multiple=.true., child=child)
+    call getSelectedAtomIndices(child, char(buffer), geom%speciesNames, geom%species, &
+        & input%indMovedAtom)
+
+  end subroutine readFilterInput
+
+
+  !> Entry point for reading convergence thresholds
+  subroutine readOptTolerance(node, input)
+
+    !> Node to get the information from
+    type(fnode), pointer, intent(in) :: node
+
+    !> Control structure to be filled
+    type(TOptTolerance), intent(out) :: input
+
+    type(fnode), pointer :: field
+    type(string) :: modifier
+
+    call getChildValue(node, "Energy", input%energy, huge(1.0_dp), modifier=modifier, child=field)
+    call convertByMul(char(modifier), energyUnits, field, input%energy)
+
+    call getChildValue(node, "GradNorm", input%gradNorm, huge(1.0_dp), modifier=modifier,&
+        & child=field)
+    call convertByMul(char(modifier), forceUnits, field, input%gradNorm)
+    call getChildValue(node, "GradElem", input%gradElem, 1.0e-4_dp, modifier=modifier, child=field)
+    call convertByMul(char(modifier), forceUnits, field, input%gradElem)
+
+    call getChildValue(node, "DispNorm", input%dispNorm, huge(1.0_dp), modifier=modifier,&
+        & child=field)
+    call convertByMul(char(modifier), lengthUnits, field, input%dispNorm)
+    call getChildValue(node, "DispElem", input%dispElem, huge(1.0_dp), modifier=modifier,&
+        & child=field)
+    call convertByMul(char(modifier), lengthUnits, field, input%dispElem)
+
+  end subroutine readOptTolerance
+
+
+  !> Entry point for reading input for FIRE
+  subroutine readFireInput(node, input)
+
+    !> Node to get the information from
+    type(fnode), pointer, intent(in) :: node
+
+    !> Control structure to be filled
+    type(TFireInput), intent(out) :: input
+
+    type(fnode), pointer :: field
+    type(string) :: modifier
+
+    call getChildValue(node, "nMin", input%nMin, 5)
+    call getChildValue(node, "aPar", input%a_start, 0.1_dp)
+    call getChildValue(node, "fInc", input%f_inc, 1.1_dp)
+    call getChildValue(node, "fDec", input%f_dec, 0.5_dp)
+    call getChildValue(node, "fAlp", input%f_alpha, 0.99_dp)
+    call getChildValue(node, "StepSize", input%dt_max, 1.0_dp, modifier=modifier, child=field)
+    call convertByMul(char(modifier), timeUnits, field, input%dt_max)
+
+  end subroutine readFireInput
+
+
+  !> Entry point for reading input for LBFGS
+  subroutine readLbfgsInput(node, input)
+
+    !> Node to get the information from
+    type(fnode), pointer, intent(in) :: node
+
+    !> Control structure to be filled
+    type(TLbfgsInput), intent(out) :: input
+
+    call getChildValue(node, "Memory", input%memory, 20)
+
+  end subroutine readLbfgsInput
+
+
+  !> Entry point for reading input for rational function optimizer
+  subroutine readRationalFuncInput(node, input)
+
+    !> Node to get the information from
+    type(fnode), pointer, intent(in) :: node
+
+    !> Control structure to be filled
+    type(TRationalFuncInput), intent(out) :: input
+
+    call getChildValue(node, "DiagLimit", input%diagLimit, 1.0e-2_dp)
+
+  end subroutine readRationalFuncInput
+
+
+end module dftbp_dftbplus_input_geoopt

src/dftbp/dftbplus/CMakeLists.txt
@@ -18,4 +18,6 @@ set(sources-fpp
   ${curdir}/specieslist.F90
   ${curdir}/transportio.F90)
 
+add_subdirectory(input)
+
 set(ALL-SOURCES-FPP ${ALL-SOURCES-FPP} ${sources-fpp} PARENT_SCOPE)

src/dftbp/dftbplus/initprogram.F90
@@ -74,7 +74,7 @@ module dftbp_dftbplus_initprogram
   use dftbp_geoopt_gdiis, only : TDIIS
   use dftbp_geoopt_geoopt, only : TGeoOpt, geoOptTypes, reset, init
   use dftbp_geoopt_lbfgs, only : TLbfgs, TLbfgs_init
-  use dftbp_geoopt_init, only : TOptimizer, TOptimizer_init, TOptConv
+  use dftbp_geoopt_package, only : TOptimizer, createOptimizer, TOptTolerance
   use dftbp_geoopt_steepdesc, only : TSteepDesc
   use dftbp_io_formatout, only : clearFile
   use dftbp_io_message, only : error, warning
@@ -602,7 +602,7 @@ module dftbp_dftbplus_initprogram
     class(TOptimizer), allocatable :: geoOpt
 
     !> Convergence thresholds for geometry optimizer
-    type(TOptConv) :: optConv
+    type(TOptTolerance) :: optTol
 
     real(dp) :: elast
     real(dp), allocatable :: gcurr(:), glast(:), displ(:)
@@ -1965,8 +1965,9 @@ contains
     if (allocated(input%ctrl%geoOpt)) then
       allocate(this%filter)
       call TFilter_init(this%filter, input%ctrl%geoOpt%filter, this%coord0, this%latVec)
-      call TOptimizer_init(this%geoOpt, input%ctrl%geoOpt, this%filter)
-      this%optConv = input%ctrl%geoOpt%conv
+      call createOptimizer(input%ctrl%geoOpt%optimizer, this%filter%getDimension(),&
+          & this%geoOpt)
+      this%optTol = input%ctrl%geoOpt%tolerance
       allocate(this%gcurr(this%filter%getDimension()))
       allocate(this%glast(this%filter%getDimension()))
       allocate(this%displ(this%filter%getDimension()))
@@ -3056,7 +3057,7 @@ contains
           write(stdOut, "(A,':',T30,A)")    "Casida solver", "Arpack"
        else
           write(stdOut, "(A,':',T30,A,i4)") "Casida solver", &
-          & "Stratmann, SubSpace: ", input%ctrl%lrespini%subSpaceFactorStratmann 
+          & "Stratmann, SubSpace: ", input%ctrl%lrespini%subSpaceFactorStratmann
        end if
     end if
 
@@ -5302,13 +5303,13 @@ contains
       end if
       if (input%ctrl%lrespini%tEnergyWindow .or. input%ctrl%lrespini%tOscillatorWindow) then
         call error("Range separated excited states not available for window options.")
-      end if 
+      end if
       if (input%ctrl%lrespini%sym == 'B' .or. input%ctrl%lrespini%sym == 'T') then
         call warning("Range separated excited states not well tested for triplet excited states!")
       end if
       if (input%ctrl%tSpin) then
         call warning("Range separated excited states not well tested for spin-polarized systems!")
-      end if 
+      end if
     else
       if (input%ctrl%lrespini%energyWindow < 0.0_dp) then
         call error("Negative energy window for excitations")

src/dftbp/dftbplus/input/CMakeLists.txt
@@ -1,0 +1,6 @@
+set(curdir ${CMAKE_CURRENT_SOURCE_DIR})
+
+list(APPEND ALL-SOURCES-FPP
+  ${curdir}/geoopt.F90)
+
+set(ALL-SOURCES-FPP ${ALL-SOURCES-FPP} PARENT_SCOPE)

src/dftbp/dftbplus/inputdata.F90
@@ -21,10 +21,10 @@ module dftbp_dftbplus_inputdata
   use dftbp_dftb_potentials, only : TAtomExtPotInput
   use dftbp_dftb_repcont, only : TRepCont
   use dftbp_dftb_slakocont, only : TSlakoCont
+  use dftbp_dftbplus_input_geoopt, only : TGeoOptInput
   use dftbp_elecsolvers_elecsolvers, only : TElectronicSolverInp
   use dftbp_extlibs_poisson, only : TPoissonInfo
   use dftbp_extlibs_tblite, only : TTBLiteInput
-  use dftbp_geoopt_input, only : TGeoOptInput
   use dftbp_io_message, only : error, warning
   use dftbp_md_xlbomd, only : TXLBOMDInp
   use dftbp_reks_reks, only : TReksInp
@@ -44,7 +44,7 @@ module dftbp_dftbplus_inputdata
   use dftbp_transport_negfvars, only : TNEGFTunDos, TNEGFGreenDensInfo, TTransPar
 #:endif
   implicit none
-  
+
   private
   public :: TControl, TSlater, TInputData, TParallelOpts
   public :: TBlacsOpts

src/dftbp/dftbplus/main.F90
@@ -1495,9 +1495,9 @@ contains
         gamax = maxval(abs(this%gcurr))
         write(stdOut, '(a)') stepSummary(energy, ediff, gnorm, gamax, dnorm, damax, 2)
 
-        econv = ediff <= epsilon(0.0_dp) .and. abs(ediff) < this%optConv%ethr
-        dconv = dnorm/this%filter%nvar < this%optConv%dthr .and. damax < this%optConv%dmax
-        gconv = gnorm/this%filter%nvar < this%optConv%gthr .and. gamax < this%optConv%gmax
+        econv = ediff <= epsilon(0.0_dp) .and. abs(ediff) < this%optTol%energy
+        dconv = dnorm / this%filter%nvar < this%optTol%dispNorm .and. damax < this%optTol%dispElem
+        gconv = gnorm / this%filter%nvar < this%optTol%gradNorm .and. gamax < this%optTol%gradElem
         converged = econv .and. gconv .and. dconv
       end block
 

src/dftbp/dftbplus/parser.F90
@@ -47,7 +47,7 @@ module dftbp_dftbplus_parser
   use dftbp_extlibs_xmlf90, only : fnode, removeChild, string, char, textNodeName, fnodeList,&
       & getLength, getNodeName, getItem1, destroyNodeList, destroyNode, assignment(=)
   use dftbp_geoopt_geoopt, only : geoOptTypes
-  use dftbp_geoopt_parser, only : readGeoOptimizer
+  use dftbp_dftbplus_input_geoopt, only : readGeoOptInput
   use dftbp_io_charmanip, only : i2c, newline, tolower, unquote
   use dftbp_io_hsdparser, only : getNodeHSdName, parseHsd
   use dftbp_io_hsdutils, only : detailedError, detailedWarning, getChild, getChildValue,&
@@ -467,7 +467,7 @@ contains
       modeName = "geometry optimization"
 
       allocate(ctrl%geoOpt)
-      call readGeoOptimizer(node, ctrl%geoOpt, geom)
+      call readGeoOptInput(node, geom, ctrl%geoOpt)
 
       ctrl%tForces = .true.
       ctrl%restartFreq = 1

src/dftbp/geoopt/CMakeLists.txt
@@ -1,19 +1,17 @@
 set(curdir ${CMAKE_CURRENT_SOURCE_DIR})
 
 set(sources-fpp
-  ${curdir}/class.F90
   ${curdir}/conjgrad.F90
   ${curdir}/filter.F90
   ${curdir}/fire.F90
   ${curdir}/gdiis.F90
   ${curdir}/geoopt.F90
-  ${curdir}/init.F90
-  ${curdir}/input.F90
   ${curdir}/lbfgs.F90
   ${curdir}/lbfgs2.F90
   ${curdir}/linemin.F90
-  ${curdir}/parser.F90
-  ${curdir}/rf.F90
+  ${curdir}/optimizer.F90
+  ${curdir}/package.F90
+  ${curdir}/rationalfunc.F90
   ${curdir}/steepdesc.F90)
 
 set(ALL-SOURCES-FPP ${ALL-SOURCES-FPP} ${sources-fpp} PARENT_SCOPE)

commit fd6d7f6b697c1c46ad21818cc029857d597624cb

Fix removed keyword in regression
The refactoring in commit 6e6c6f0841 did not fix
geoopt/Si_2_lattice_rf for removal of GradAMax. The rational function
optimizer also requires defaults from parser in the Convergence{}
block.

src/dftbp/dftbplus/input/geoopt.F90
@@ -73,9 +73,10 @@ contains
     call readFilterInput(node, geom, input%filter, atomsRange)
 
     call getChild(node, "Convergence", child, requested=.false.)
-    if (associated(child)) then
-      call readOptTolerance(child, input%tolerance)
+    if (.not.associated(child)) then
+      call setChild(node, "Convergence", child)
     end if
+    call readOptTolerance(child, input%tolerance)
 
     call getChildValue(node, "MaxSteps", input%nGeoSteps, 20*geom%nAtom)
 

test/app/dftb+/geoopt/Si_2_lattice_rf/_autotest.tag
@@ -1,12 +1,22 @@
+cell_volume         :real:0:
+  0.274553547342934E+003
+orbital_charges     :real:2:4,2
+  0.152098711019808E+001  0.826328596746008E+000  0.826336682148980E+000
+  0.826347610906955E+000  0.152098711019808E+001  0.826328596746008E+000
+  0.826336682148981E+000  0.826347610906955E+000
 forces              :real:2:3,2
-  0.335751910603964E-004  0.490031299509986E-004 -0.527407441029580E-004
- -0.335751910603964E-004 -0.490031299509986E-004  0.527407441029580E-004
+  0.110573834547405E-006 -0.212376279555263E-006  0.100873730228910E-007
+ -0.110573834547405E-006  0.212376279555263E-006 -0.100873730228910E-007
 stress              :real:2:3,3
- -0.305753131109809E-006  0.169356740845800E-006 -0.527992124738629E-006
-  0.169282066683262E-006  0.799631292684134E-006  0.352490811936655E-006
- -0.528029990819514E-006  0.352523493778947E-006 -0.209369858913133E-006
+ -0.161230589785426E-007  0.791417844534615E-008 -0.259254840048934E-008
+  0.791417282649697E-008  0.340134984578514E-007  0.129708334165320E-007
+ -0.259256441341376E-008  0.129708377800300E-007 -0.206734695177733E-008
 mermin_energy       :real:0:
- -0.259536875144452E+001
+ -0.259536882768338E+001
 end_coords          :real:2:3,2
- -0.644888415310459E-001  0.121454279766786E+000  0.421527073104856E-001
-  0.250223536679786E+001  0.271313470310160E+001  0.262236093658580E+001
+ -0.642733082156862E-001  0.121230577444066E+000  0.421253871525171E-001
+  0.250201983348250E+001  0.271335840542432E+001  0.262238825674376E+001
+dipole_moments      :real:2:3,1
+ -0.681809391290664E-013 -0.793454320083036E-013 -0.745607813704761E-013
+scaled_dipole       :real:2:3,1
+ -0.681809391290664E-013 -0.793454320083036E-013 -0.745607813704761E-013

test/app/dftb+/geoopt/Si_2_lattice_rf/dftb_in.hsd
@@ -8,17 +8,20 @@ Geometry = GenFormat {
  0.0  3.0  3.0
  3.0  0.0  3.0
 }
+
 Driver = GeometryOptimization {
  Optimizer = Rational {}
  MovedAtoms = {1 2}
  LatticeOpt = Yes
- Convergence = { GradAMax = 1.0E-8 }
+ Convergence = { GradElem = 1.0E-6 }
+ MaxSteps = 200
 }
+
 Hamiltonian = DFTB {
   MaxAngularMomentum = {
     Si = "p"
   }
-  Eigensolver = Standard {}
+  Solver = QR {}
   Filling = Fermi {}
   SlaterKosterFiles = {
     Si-Si = "slakos/origin/pbc-0-3/Si-Si.skf"
@@ -30,11 +33,11 @@ Hamiltonian = DFTB {
    0.5 0.5 0.5
   }
 }
+
 Options = { WriteAutotestTag = Yes }
 
 ParserOptions = {
-  ParserVersion = 3
+  ParserVersion = 12
 }
 
 Parallel { UseOmpThreads = Yes }
-