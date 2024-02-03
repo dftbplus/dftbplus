186be49e49693ff1a12570c1d19df779517ed590
Justin Bich <ju_bi1@uni-bremen.de>
2024-02-03 16:27:45 +0100

    Add new headers

--- test/app/dftb+/legacy/file_access/split.py
+++ test/app/dftb+/legacy/file_access/split.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+#------------------------------------------------------------------------------#
+#  DFTB+: general package for performing fast atomistic simulations            #
+#  Copyright (C) 2006 - 2024  DFTB+ developers group                           #
+#                                                                              #
+#  See the LICENSE file for terms of usage and distribution.                   #
+#------------------------------------------------------------------------------#
+
 
 with open("dftb_in.hsd.all") as inp:
     for ii, chunk in enumerate(inp.read().split("%")):
