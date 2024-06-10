1648931b7fdabdd7e9023852f621fc0083cfe6da
Thomas Niehaus <thomas.niehaus@univ-lyon1.fr>
2024-06-10 15:20:06 +0200

    Update src/dftbp/timedep/linrespgrad.F90
    Co-authored-by: Ben Hourahine <bhourahine@gmail.com>

--- src/dftbp/timedep/linrespgrad.F90
+++ src/dftbp/timedep/linrespgrad.F90
@@ -5657,7 +5657,7 @@ contains
   !> Computes full range part of H^+_pq [M] as defined in Furche JCP 117 7433 (2002) eq. 20
   !> Here pq are arbitrary orbitals and M is a general matrix with ov,oo,vv components
   !! iMode = 1: returns oo components of H
-  !> iMode = 2: returns vv components of H
+  !! iMode = 2: returns vv components of H
   !> iMode = 3: returns ov components of H
   !> Note: The full range part of H^- is zero! 
   !> Routine currently does not work for M_ia /= 0 on entry!
