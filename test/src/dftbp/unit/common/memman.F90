09ec42beb50b31993c65a924f04b36060e414aeb
Bálint Aradi <aradi@uni-bremen.de>
2024-08-16 21:41:53 +0200

    Base unit tests on Fortuno and rename unit test folder
    Uses Fortuno v0.1.0.

---
+++ test/src/dftbp/unit/common/memman.F90
@@ -0,0 +1,114 @@
+!--------------------------------------------------------------------------------------------------!
+!  DFTB+: general package for performing fast atomistic simulations                                !
+!  Copyright (C) 2006 - 2025  DFTB+ developers group                                               !
+!                                                                                                  !
+!  See the LICENSE file for terms of usage and distribution.                                       !
+!--------------------------------------------------------------------------------------------------!
+
+#:include "fortuno_serial.fypp"
+
+module test_common_memman
+  use dftbp_common_accuracy, only : dp
+  use dftbp_common_memman, only : TAlignedArray
+  use fortuno_serial, only : suite => serial_suite_item, test_list
+  $:FORTUNO_SERIAL_IMPORTS()
+  implicit none
+
+  private
+  public :: tests
+
+contains
+
+
+  $:TEST("allocate32Bytes")
+    type(TAlignedArray) :: aligned
+    real(dp), pointer :: array(:)
+
+    call aligned%allocate(7, 32)
+    call aligned%getArray(array)
+
+    @:ASSERT(associated(array))
+    @:ASSERT(aligned%alignment == 32)
+
+    @:ASSERT(aligned%size == 7)
+    @:ASSERT(size(array) == 7)
+    @:ASSERT(size(array, dim=1) == 7)
+    @:ASSERT(lbound(array, dim=1) == 1)
+    @:ASSERT(ubound(array, dim=1) == 7)
+
+    array(:) = 1.0_dp
+    array(7) = 42.0_dp
+    nullify(array)
+    call aligned%getArray(array)
+    @:ASSERT(array(7) == 42.0_dp)
+  $:END_TEST()
+
+
+  $:TEST("allocateDefault64Bytes")
+    type(TAlignedArray) :: aligned
+    real(dp), pointer :: array(:)
+
+    call aligned%allocate(7)
+    call aligned%getArray(array)
+
+    @:ASSERT(associated(array))
+    @:ASSERT(aligned%alignment == 64)
+  $:END_TEST()
+
+
+  $:TEST("deallocate")
+    type(TAlignedArray) :: aligned
+    real(dp), pointer :: array(:)
+
+    call aligned%getArray(array)
+    @:ASSERT(.not. associated(array))
+
+    call aligned%allocate(7)
+    call aligned%getArray(array)
+    @:ASSERT(associated(array))
+
+    call triggerDeallocation_(aligned)
+    call aligned%getArray(array)
+    @:ASSERT(.not. associated(array))
+  $:END_TEST()
+
+
+  $:TEST("reallocate")
+    type(TAlignedArray) :: aligned, aligned2
+    real(dp), pointer :: array(:)
+
+    call aligned%allocate(3)
+    call aligned%getArray(array)
+    array(1) = 3.0_dp
+
+    call aligned2%allocate(4)
+    call aligned2%getArray(array)
+    array(1) = 4.0_dp
+
+    aligned = aligned2
+    @:ASSERT(aligned%size == 4)
+    call aligned%getArray(array)
+    @:ASSERT(array(1) == 4.0_dp)
+  $:END_TEST()
+
+
+  function tests()
+    type(test_list) :: tests
+
+    tests = test_list([&
+        suite("memman", test_list([&
+            $:TEST_ITEMS()
+        ]))&
+    ])
+    $:STOP_ON_MISSING_TEST_ITEMS()
+
+  end function tests
+
+
+  subroutine triggerDeallocation_(instance)
+
+    type(TAlignedArray), intent(out) :: instance
+
+  end subroutine triggerDeallocation_
+
+end module test_common_memman
---
+++ test/src/dftbp/unit/math/matrixops.F90
@@ -0,0 +1,83 @@
+!--------------------------------------------------------------------------------------------------!
+!  DFTB+: general package for performing fast atomistic simulations                                !
+!  Copyright (C) 2006 - 2025  DFTB+ developers group                                               !
+!                                                                                                  !
+!  See the LICENSE file for terms of usage and distribution.                                       !
+!--------------------------------------------------------------------------------------------------!
+
+#:include "fortuno_serial.fypp"
+
+module test_math_matrixops
+  use dftbp_common_accuracy, only : dp
+  use dftbp_math_matrixops, only : adjointLowerTriangle
+  use fortuno_serial, only : suite => serial_suite_item, test_list
+  $:FORTUNO_SERIAL_IMPORTS()
+  implicit none
+
+  private
+  public :: tests
+
+
+  ! Matrix dimension
+  integer, parameter :: nn = 3
+
+
+contains
+
+
+  $:TEST("real")
+    real(dp) :: matReal(nn, nn)
+    integer :: ii, jj, kk
+
+    matReal(:,:) = 0.0_dp
+    kk = 0
+    do ii = 1, size(matReal, dim=1)
+      do jj = ii, size(matReal, dim=2)
+        kk = kk + 1
+        matReal(jj, ii) = real(kk, dp)
+      end do
+    end do
+    call adjointLowerTriangle(matReal)
+    @:ASSERT(all(matReal > epsilon(0.0_dp)))
+    @:ASSERT(all(abs(matReal - transpose(matReal)) < epsilon(0.0_dp)))
+  $:END_TEST()
+
+
+  $:TEST("complex")
+    complex(dp) :: matCplx(nn, nn)
+    integer :: ii, jj, kk
+
+    matCplx(:,:) = cmplx(0, 0, dp)
+    kk = 0
+    do ii = 1, size(matCplx, dim=1)
+      do jj = ii, size(matCplx, dim=2)
+        kk = kk + 1
+        matCplx(jj, ii) = real(kk, dp)
+      end do
+    end do
+    kk = 0
+    do ii = 1, size(matCplx, dim=1)
+      do jj = ii + 1, size(matCplx, dim=2)
+        kk = kk + 1
+        matCplx(jj, ii) = matCplx(jj, ii) + cmplx(0, kk, dp)
+      end do
+    end do
+    call adjointLowerTriangle(matCplx)
+    @:ASSERT(all(abs(matCplx) > epsilon(0.0_dp)))
+    @:ASSERT(all(abs(matCplx - transpose(conjg(matCplx))) < epsilon(0.0_dp)))
+  $:END_TEST()
+
+
+  function tests()
+    type(test_list) :: tests
+
+    tests = test_list([&
+        suite("matrixops", test_list([&
+            $:TEST_ITEMS()
+        ]))&
+    ])
+    $:STOP_ON_MISSING_TEST_ITEMS()
+
+  end function tests
+
+end module test_math_matrixops
