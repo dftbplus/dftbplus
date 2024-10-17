2dcfa2300b3c1a3da810a11a451e3e9f0ce4a707
Matias Berdakin <matiasberdakin@unc.edu.ar>
2024-10-17 09:39:23 +0200

    Parallelize (MPI) rt-TD-DFTB time-propagation
    Co-authored-by: Franco Bonafe <fbonafe@unc.edu.ar>

--- src/dftbp/timedep/dynamicsrestart.F90
+++ src/dftbp/timedep/dynamicsrestart.F90
@@ -11,8 +11,14 @@
 !> Routines for the restart of the time propagation of the density matrix/atoms
 module dftbp_timedep_dynamicsrestart
   use dftbp_common_accuracy, only : dp
+  use dftbp_common_environment, only : TEnvironment
   use dftbp_common_file, only : TFileDescr, TOpenOptions, openFile, closeFile
   use dftbp_common_status, only : TStatus
+  #:if WITH_SCALAPACK
+  use dftbp_extlibs_scalapackfx, only : linecomm, DLEN_, M_, N_, scalafx_getdescriptor
+  use dftbp_type_densedescr, only: TDenseDescr
+  use dftbp_type_commontypes, only : TParallelKS
+  #:endif
   implicit none
 
   !> Version number for restart format, please increment if you change the file format (and consider
@@ -21,7 +27,10 @@ module dftbp_timedep_dynamicsrestart
 
   private
   public :: writeRestartFile, readRestartFile
-
+  #:if WITH_SCALAPACK
+  public :: writeRestartFileBlacs, readRestartFileBlacs
+  #:endif
+  
 contains
 
   !> Write to a restart file.
@@ -260,5 +269,179 @@ contains
     call closeFile(fd)
 
   end subroutine readRestartFile
+
+
+  #:if WITH_SCALAPACK
+  !> Write to a restart file the DM in distributed format
+  subroutine writeRestartFileBlacs(rho, rhoOld, coord, veloc, time, dt, fileName, env, &
+      & denseDesc, parallelKS, errStatus)
+
+    !> Density matrix
+    complex(dp), intent(in) :: rho(:,:,:)
+
+    !> Density matrix at previous time step
+    complex(dp), intent(in) :: rhoOld(:,:,:)
+
+    !> Atomic coordinates
+    real(dp), intent(in) :: coord(:,:)
+
+    !> Atomic velocities
+    real(dp), intent(in) :: veloc(:,:)
+
+    !> Simulation time (in atomic units)
+    real(dp), intent(in) :: time
+
+    !> Time step being used (in atomic units)
+    real(dp), intent(in) :: dt
+
+    !> Name of the dump file
+    character(len=*), intent(in) :: fileName
+
+    !> Environment settings
+    type(TEnvironment), intent(inout) :: env
+
+    !> Dense matrix descriptor
+    type(TDenseDescr), intent(in) :: denseDesc
+
+    !> Container for parallel distribution pattern of k-points and spins
+    type(TParallelKS), allocatable :: parallelKS
+
+    !> Operation status
+    type(TStatus), intent(out) :: errStatus
+
+    type(TFileDescr) :: fd
+    integer :: iErr, nOrb, nSpin, icol, iKS, irow, icoll
+    type(linecomm) :: collector
+    complex(dp), allocatable :: localRhoCol(:)
+
+    nOrb = denseDesc%fullSize
+    nSpin = parallelKS%nLocalKS
+    if (env%mpi%tGlobalLead) then
+      call openFile(fd, trim(fileName) // '.bin',&
+          & options=TOpenOptions(form='unformatted', access='stream', action='write'))
+      write(fd%unit) iDumpFormat
+      write(fd%unit) nOrb, nSpin, size(coord, dim=2), time, dt
+      write(fd%unit) coord, veloc
+    end if
+
+    nOrb = denseDesc%fullSize
+    allocate(localRhoCol(nOrb))
+
+    call collector%init(env%blacs%orbitalGrid, denseDesc%blacsOrbSqr, "c")
+    do iKS = 1, parallelKS%nLocalKS
+      do icol = 1, nOrb
+        if (env%mpi%tGlobalLead) then
+          call collector%getline_lead(env%blacs%orbitalGrid, icol, rho(:,:,iKS), localRhoCol)
+          write(fd%unit)localRhoCol(:)
+        else
+          call collector%getline_follow(env%blacs%orbitalGrid, icol, rho(:,:,iKS))
+        end if
+      end do
+    end do
+
+    do iKS = 1, parallelKS%nLocalKS
+      do icol = 1, nOrb
+        if (env%mpi%tGlobalLead) then
+          call collector%getline_lead(env%blacs%orbitalGrid, icol, rhoOld(:,:,iKS), localRhoCol)
+          write(fd%unit)localRhoCol(:)
+        else
+          call collector%getline_follow(env%blacs%orbitalGrid, icol, rhoOld(:,:,iKS))
+        end if
+      end do
+    end do
+
+    if (env%mpi%tGlobalLead) then
+      call closeFile(fd)
+    end if
+
+  end subroutine writeRestartFileBlacs
+
+
+  !> Write to a restart file the DM in Blacs format
+  subroutine readRestartFileBlacs(rho, rhoOld, coord, veloc, time, dt, fileName, env, &
+      & denseDesc, parallelKS, errStatus)
+
+    !> Density Matrix
+    complex(dp), intent(out) :: rho(:,:,:)
+
+    !> Previous density Matrix
+    complex(dp), intent(out) :: rhoOld(:,:,:)
+
+    !> Atomic coordinates
+    real(dp), intent(out) :: coord(:,:)
+
+    !> Previous simulation elapsed time until restart file writing
+    real(dp), intent(out) :: time
+
+    !> Time step being currently used (in atomic units) for checking compatibility
+    real(dp), intent(in) :: dt
+
+    !> Name of the file to open
+    character(*), intent(in) :: fileName
+
+    !> Atomic velocities
+    real(dp), intent(out) :: veloc(:,:)
+
+    !> Environment settings
+    type(TEnvironment), intent(inout) :: env
+
+    !> Dense matrix descriptor
+    type(TDenseDescr), intent(in) :: denseDesc
+
+    !> Container for parallel distribution pattern of k-points and spins
+    type(TParallelKS), allocatable :: parallelKS
+
+    !> Operation status
+    type(TStatus), intent(out) :: errStatus
+
+    type(TFileDescr) :: fd
+    integer :: iErr, version, nOrb, nSpin, nAtom, icol, iKS, irow, icoll
+    character(len=120) :: error_string
+    real(dp) :: deltaT
+    type(linecomm) :: distributor
+    complex(dp), allocatable :: localRhoCol(:)
+
+    if (env%mpi%tGlobalLead) then
+      call openFile(fd, file=trim(fileName)//'.bin',&
+          & options=TOpenOptions(form='unformatted', access='stream', action='read',&
+          & position="rewind"))
+
+      read(fd%unit) version
+      read(fd%unit) nOrb, nSpin, nAtom, time, deltaT
+      read(fd%unit) coord, veloc
+    end if
+
+    nOrb = denseDesc%fullSize
+    allocate(localRhoCol(nOrb))
+
+    call distributor%init(env%blacs%orbitalGrid, denseDesc%blacsOrbSqr, "c")
+    do iKS = 1, parallelKS%nLocalKS
+      do icol = 1, nOrb
+        if (env%mpi%tGlobalLead) then
+          read(fd%unit) localRhoCol(:)
+          call distributor%setline_lead(env%blacs%orbitalGrid, icol, localRhoCol(:), rho(:,:,iKS))
+        else
+          call distributor%setline_follow(env%blacs%orbitalGrid, icol, rho(:,:,iKS))
+        end if
+      end do
+    end do
+
+    do iKS = 1, parallelKS%nLocalKS
+      do icol = 1, nOrb
+        if (env%mpi%tGlobalLead) then
+          read(fd%unit) localRhoCol(:)
+          call distributor%setline_lead(env%blacs%orbitalGrid, icol, localRhoCol(:), rhoOld(:,:,iKS))
+        else
+          call distributor%setline_follow(env%blacs%orbitalGrid, icol, rhoOld(:,:,iKS))
+        end if
+      end do
+    end do
+
+    if (env%mpi%tGlobalLead) then
+      call closeFile(fd)
+    end if
+
+  end subroutine readRestartFileBlacs
+  #:endif
 
 end module dftbp_timedep_dynamicsrestart
