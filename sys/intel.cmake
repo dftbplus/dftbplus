15e4df44c2b5351da1c9e82d49ddde2af2e64f55
Bálint Aradi <aradi@uni-bremen.de>
2025-02-11 12:29:48 +0100

    Disable broken boundary checking for Intel compiler

--- sys/intel.cmake
+++ sys/intel.cmake
@@ -30,7 +30,9 @@ set(Fortran_FLAGS_RELWITHDEBINFO "-g ${Fortran_FLAGS_RELEASE}"
   CACHE STRING "Fortran compiler flags for Release build")
 
 # Note: uninit only works reliably if all linked libraries were compiled using this flag
-set(Fortran_FLAGS_DEBUG "-g -O0 -warn all -stand f08 -check all,nouninit -diag-error-limit 1 -traceback"
+# Note: boundary violation check is broken in ifx 2025.0
+# (https://community.intel.com/t5/Intel-Fortran-Compiler/Compiler-bug-boundary-check-triggering-false-positives/m-p/1664953#M175032)
+set(Fortran_FLAGS_DEBUG "-g -O0 -warn all -stand f08 -check all,nouninit,nobounds -diag-error-limit 1 -traceback"
   CACHE STRING "Fortran compiler flags for Debug build")
 
 # Use intrinsic Fortran 2008 erf/erfc functions
