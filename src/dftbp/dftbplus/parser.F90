5181843f0cc28dbcce9eab310f0d969da8be09ac
Tammo van der Heide <vanderhe@uni-bremen.de>
2025-02-21 15:08:19 +0100

    Switch the default HFX algo Thresholded -> MatrixBased

--- src/dftbp/dftbplus/parser.F90
+++ src/dftbp/dftbplus/parser.F90
@@ -7982,7 +7982,7 @@ contains
 
       allocate(input)
       input%hybridXcType = hybridXcSkType
-      call getChildValue(hybridValue, "Screening", screeningValue, "Thresholded",&
+      call getChildValue(hybridValue, "Screening", screeningValue, "MatrixBased",&
           & child=screeningChild)
 
       call getNodeName(screeningValue, buffer)
