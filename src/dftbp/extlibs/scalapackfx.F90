71cdb89a7373521e0445fc163e7f78780eae50ce
Tobias Melson <tobias.melson@mpcdf.mpg.de>
2025-09-12 16:06:37 +0200

    Implemented matrix redistribution in ELPA

--- src/dftbp/extlibs/scalapackfx.F90
+++ src/dftbp/extlibs/scalapackfx.F90
@@ -11,7 +11,7 @@
 module dftbp_extlibs_scalapackfx
 #:if WITH_SCALAPACK
   use libscalapackfx_module, only : blacsfx_gemr2d, blacsfx_gsum, blacsgrid, blocklist, CSRC_,&
-      & DLEN_, linecomm, M_, MB_, N_, NB_, pblasfx_pgemm, pblasfx_phemm, pblasfx_psymm,&
+      & CTXT_, DLEN_, linecomm, M_, MB_, N_, NB_, pblasfx_pgemm, pblasfx_phemm, pblasfx_psymm,&
       & pblasfx_psymv, pblasfx_ptran, pblasfx_ptranc, RSRC_, scalafx_addg2l, scalafx_addl2g,&
       & scalafx_cpg2l, scalafx_cpl2g, scalafx_getdescriptor, scalafx_getlocalshape,&
       & scalafx_indxl2g, scalafx_infog2l, scalafx_islocal, scalafx_numroc, scalafx_pgetrf,&
