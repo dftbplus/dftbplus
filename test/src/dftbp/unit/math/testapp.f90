bc6a04e4cfc2b147ca41a8083609db621fc72d6e
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2025-11-24 18:52:02 +0000

    SVD clean-up
    Added regression test for the real solver in a couple of modes.
    
    Note, mixing asserts inside the lapack routine with the regression
    tests with Fortuno currently cause a segfault.

--- test/src/dftbp/unit/math/testapp.f90
+++ test/src/dftbp/unit/math/testapp.f90
@@ -1,8 +1,13 @@
 program testapp
-  use fortuno_serial, only : execute_serial_cmd_app
+  use fortuno_serial, only : execute_serial_cmd_app, test_list
   use test_math_matrixops, only : matrixops_tests => tests
+  use test_math_lapackroutines, only : lapack_svd_tests => tests
   implicit none
 
-  call execute_serial_cmd_app(matrixops_tests())
+  call execute_serial_cmd_app(test_list([&
+      matrixops_tests(),&
+      lapack_svd_tests()&
+    ])&
+  )
 
 end program testapp
