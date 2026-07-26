db4a67954493e956d7c283543b51ace93e7e1749
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2026-07-26 22:58:24 +0100

    Improve numerical accuracy of filling
    * Use compensated sums in the filling routines
    
    * Remove root polishing as tolerance too short and it never gets
      called.

--- src/dftbp/dftb/etemp.F90
+++ src/dftbp/dftb/etemp.F90
@@ -8,10 +8,9 @@
 #:include 'common.fypp'
 
 !> Contains routines related to finite electron temperature, including Fermi, Gaussian and
-!> Methfessel-Paxton broadening functions.
-!> To do: Add other methods, including possibly Pederson and Jackson method
-!> PRB 43, 7312 (1991). Also fix exact occupation for electron numers, using
-!> interpolation instead of bisection.
+!! Methfessel-Paxton broadening functions.
+!! To do: Add other methods, including possibly Pederson and Jackson method PRB 43, 7312
+!! (1991). Also fix exact occupation for electron numers, using interpolation instead of bisection.
 module dftbp_dftb_etemp
   use dftbp_common_accuracy, only : dp, elecTol, elecTolMax, mExpArg, rsp
   use dftbp_common_constants, only : pi
@@ -20,6 +19,7 @@ module dftbp_dftb_etemp
   use dftbp_math_factorial, only : fact
   use dftbp_math_hermite, only : hx
   use dftbp_math_sorting, only : index_heap_sort
+  use dftbp_math_summation, only : kahan
   implicit none
 
   private
@@ -34,7 +34,7 @@ module dftbp_dftb_etemp
     integer :: Gaussian = 1
 
     !> Definition of a type of broadening function - Methfessel-Paxton, for higher orders use
-    !> Methfessel + n as a value
+    !! Methfessel + n as a value
     integer :: Methfessel = 2
 
   end type TFillingTypesEnum
@@ -49,13 +49,13 @@ contains
 
 
   !> Driver to calculate electron filling, the band-structure energy at T and extrapolated to T=0K,
-  !> and the entropy of the electron energy for the Mermin free energy, returning band energy and
-  !> entropy for each channel but common Fermi level
-  !>
-  !> Note: use slices of eigenvalues and other input arrays if you want a different Fermi energy and
-  !> filling for different k-points and/or spins.
-  !>
-  !> Note: If no electrons are present, the Fermi energy is set to zero per default.
+  !! and the entropy of the electron energy for the Mermin free energy, returning band energy and
+  !! entropy for each channel but common Fermi level
+  !!
+  !! Note: use slices of eigenvalues and other input arrays if you want a different Fermi energy and
+  !! filling for different k-points and/or spins.
+  !!
+  !! Note: If no electrons are present, the Fermi energy is set to zero per default.
   subroutine Efilling(Ebs, Ef, TS, E0, filling, eigenvals, nElectrons, kT, kWeight, distrib)
 
     !> Band structure energy at T
@@ -85,15 +85,13 @@ contains
     !> The k-point weightings
     real(dp), intent(in) :: kWeight(:)
 
-    !> Choice of distribution functions, currently Fermi, Gaussian and Methfessle-Paxton
-    !> supported. The flags is defined symbolically, so (Methfessel + 2) gives the 2nd order M-P
-    !> scheme
+    !> Choice of distribution functions, currently Fermi, Gaussian and Methfessle-Paxton supported.
+    !! The flags is defined symbolically, so (Methfessel + 2) gives the 2nd order M-P scheme
     integer, intent(in) :: distrib
 
     real(dp) :: upperEf, lowerEf
     real(dp) :: nElec
     real(dp) :: nElecMax, nElecMin, maxEig, minEig
-    real(dp) :: EfOld
 
     @:ASSERT(all(shape(filling) == shape(eigenvals)))
     @:ASSERT(size(eigenvals, dim=3) == size(Ebs))
@@ -182,34 +180,11 @@ contains
     end if
 
     nElec = electronCount(Ef, eigenvals, kT, distrib, kWeight)
-    ! Polish resulting root with Newton-Raphson type steps
-    if (abs(nElectrons - nElec) > elecTol) then
-      if (distrib == fillingTypes%Fermi) then ! only derivs for Fermi so far
-        if (abs(derivElectronCount(Ef, eigenvals, kT, distrib, kWeight)) >= epsilon(1.0_dp)) then
-          EfOld = Ef
-          Ef = Ef - (electronCount(Ef, eigenvals, kT, distrib, kWeight) - nElectrons)&
-              & / derivElectronCount(Ef, eigenvals, kT, distrib, kWeight)
-          do while(abs(electronCount(EfOld, eigenvals, kT, distrib, kWeight) - nElectrons)&
-              & > abs(electronCount(Ef, eigenvals, kT, distrib, kWeight) - nElectrons))
-            if (abs(derivElectronCount(Ef, eigenvals, kT, distrib, kWeight))&
-                & >= epsilon(1.0_dp)) then
-              EfOld = Ef
-              Ef = Ef - (electronCount(Ef, eigenvals, kT, distrib, kWeight) - nElectrons)&
-                  & / derivElectronCount(Ef, eigenvals, kT, distrib, kWeight)
-            else
-              exit
-            end if
-          end do
-          Ef = EfOld
-        end if
-      end if
-    end if
 
-    nElec = electronCount(Ef, eigenvals, kT, distrib, kWeight)
-    call electronFill(Ebs,filling,TS,E0,Ef,eigenvals,kT,distrib,kWeight)
+    call electronFill(Ebs, filling, TS, E0, Ef, eigenvals, kT, distrib, kWeight)
 
-    ! re-scale to give exact number of electrons, this is a temporay hack
-    if (nElec > epsilon(1.0_dp)) then
+    ! Re-scale to give exact number of electrons, this is a temporay hack
+    if (nElectrons > epsilon2 .and. abs(nElectrons - nElec) > epsilon2) then
       filling(:,:,:) = filling * nElectrons / nElec
     end if
 
@@ -217,6 +192,7 @@ contains
 
 
   !> Calculates the number of electrons for a given Fermi energy and distribution function
+  !! Totals are made using using Kahan–Babušk summation
   function electronCount(Ef,eigenvals,kT,distrib,kWeight)
 
     !> Electrons for this Fermi energy
@@ -232,136 +208,89 @@ contains
     real(dp), intent(in) :: kT
 
     !> Choice of distribution functions, currently Fermi, Gaussian and Methfessle-Paxton
-    !> supported. The flags is defined symbolically, so (Methfessel + 2) gives the 2nd order M-P
-    !> scheme
+    !! supported. The flags is defined symbolically, so (Methfessel + 2) gives the 2nd order M-P
+    !! scheme
     integer, intent(in) :: distrib
 
     !> The k-point weightings
     real(dp), intent(in) :: kWeight(:)
 
-    integer :: MPorder
-    real(dp) :: w
-    real(dp), allocatable :: A(:)
-    real(dp), allocatable :: hermites(:)
-    integer i, j , k, l, ispin
-    real(dp) :: occ, x
+    real(dp), allocatable :: A(:), hermites(:)
+    integer ii, jj , kk, ll, MPorder, iSpin
+    real(dp) :: beta, occ, comp, x
 
-    w = 1.0_dp/kT
-    electronCount=0.0_dp
+    beta = 1.0_dp / kT
+    electronCount = 0.0_dp
+    comp = 0.0_dp
+
     if (distrib /= fillingTypes%Fermi) then
+
       MPorder = distrib - fillingTypes%Methfessel
       allocate(A(0:MPorder))
       allocate(hermites(0:2*MPorder))
       call Aweights(A,MPorder)
-      do ispin = 1, size(eigenvals,dim=3)
-        do i = 1, size(kWeight)
-          do j = 1, size(eigenvals,dim=1)
-            if (eigenvals(j,i,ispin)>(Ef-3.0_dp*w)) then
-              exit
+      do iSpin = 1, size(eigenvals,dim=3)
+        do ii = 1, size(kWeight)
+
+          lpFilled: do jj = 1, size(eigenvals, dim=1)
+            if (eigenvals(jj, ii, iSpin) > Ef - 3.0_dp * beta) then
+              ! Should find with bisection insead of an if statements inside a loop
+              exit lpFilled
             else
-              electronCount=electronCount+kWeight(i)
+              call Kahan(electronCount, kWeight(ii), comp)
             end if
-          end do
-          do k = j, size(eigenvals,dim=1)
-            if (eigenvals(k,i,ispin)>(Ef+3.0_dp*w)) then
-              exit
+          end do lpFilled
+
+          lpPartially: do kk = jj, size(eigenvals, dim=1)
+            if (eigenvals(kk, ii, iSpin) > Ef + 3.0_dp * beta) then
+              ! Likewise, find with bisection, not an if test
+              exit lpPartially
             end if
-            x = ( eigenvals(k,i,ispin) - Ef ) / kT
-            call hX(hermites,MPorder*2,x)
+            x = (eigenvals(kk, ii, iSpin) - Ef) * beta
+            call hX(hermites, MPorder*2, x)
             occ = 0.5_dp * erfcwrap(x)
-            do l=1, MPorder
-              occ = occ + A(l) * hermites(2*l-1) * exp(-x**2)
+            do ll = 1, MPorder
+              occ = occ + A(ll) * hermites(2 * ll - 1) * exp(-x**2)
             end do
-            electronCount = electronCount + occ * kWeight(i)
-          end do
+            call kahan(electronCount, occ * kWeight(ii), comp)
+          end do lpPartially
+
         end do
       end do
+
     else
-      do ispin = 1, size(eigenvals,dim=3)
-        do i = 1, size(kWeight)
-          do j = 1, size(eigenvals,dim=1)
-            x = ( eigenvals(j,i,ispin) - Ef ) / kT
+
+      ! Fermi function
+      do iSpin = 1, size(eigenvals, dim=3)
+        do ii = 1, size(kWeight)
+          do jj = 1, size(eigenvals, dim=1)
+            x = ( eigenvals(jj, ii, iSpin) - Ef ) * beta
             ! Where the compiler does not handle inf gracefully, trap the exponential function for
             ! small input values
           #:if EXP_TRAP
             if (x <= mExpArg) then
-              electronCount = electronCount + kWeight(i)/(1.0_dp + exp(x))
+              call kahan(electronCount, kWeight(ii) / (1.0_dp + exp(x)), comp)
             end if
           #:else
-            electronCount = electronCount + kWeight(i)/(1.0_dp + exp(x))
+            call kahan(electronCount, kWeight(ii) / (1.0_dp + exp(x)), comp)
           #:endif
           end do
         end do
       end do
+
     end if
+
+    electronCount = electronCount + comp
+
   end function electronCount
 
 
-  !> Calculates the derivative of the number of electrons for a given Fermi energy and distribution
-  !> function
-  !>
-  !> To do: support Methfestle-Paxton
-  function derivElectronCount(Ef,eigenvals,kT,distrib,kWeight)
-
-    !> Derivative of electrons wrt to Ef
-    real(dp) :: derivElectronCount
-
-    !> Fermi energy for given distribution
-    real(dp), intent(in) :: Ef
-
-    !> The eigenvalues of the levels, 1st index is energy
-    !> 2nd index is k-point and 3nd index is spin
-    real(dp), intent(in) :: eigenvals(:,:,:)
-
-    !> Thermal energy in atomic units
-    real(dp), intent(in) :: kT
-
-    !> Choice of distribution functions, currently
-    integer, intent(in) :: distrib
-
-    !> Fermi supported.
-    real(dp), intent(in) :: kWeight(:)
-
-    !> The k-point weightings
-    real(dp) :: w
-    integer i, j, ispin
-    real(dp) :: x
-
-    w = 1.0_dp/kT
-    derivElectronCount=0.0_dp
-    if (distrib /= fillingTypes%Fermi) then
-      call error("Fermi distribution only supported")
-    else
-      do ispin = 1, size(eigenvals,dim=3)
-        do i = 1, size(kWeight)
-          do j = 1, size(eigenvals,dim=1)
-            x = ( eigenvals(j,i,ispin) - Ef ) * w
-            if (x<10.0_dp) then
-              ! Where the compiler does not handle inf gracefully, trap the exponential function for
-              ! small input values
-            #:if EXP_TRAP
-              if (x <= mExpArg) then
-                derivElectronCount = derivElectronCount + &
-                    & (w*kWeight(i)) * (exp(x)/((1.0_dp + exp(x))**2))
-              end if
-            #:else
-              derivElectronCount = derivElectronCount + &
-                  & (w*kWeight(i)) * (exp(x)/((1.0_dp + exp(x))**2))
-            #:endif
-            end if
-          end do
-        end do
-      end do
-    end if
-  end function derivElectronCount
-
-
   !> Calculate filling and TS for the given eigenspectrum and distribution function and Fermi
-  !> energy, for two spin channels
-  !>
-  !> Ref: G. Kresse and J. Furthm&uuml;ller, Phys. Rev. B vol 54, pp 11169 (1996).
-  !> Ref: M. Methfessel and A. T. Paxton,, Phys. Rev. B vol 40, pp 3616 (1989).
-  !> Ref: F. Wagner, Th. Laloyaux and M. Scheffler, Phys. Rev. B, vol 57 pp 2102 (1998).
+  !! energy, for two spin channels
+  !!
+  !! Ref: G. Kresse and J. Furthm&uuml;ller, Phys. Rev. B vol 54, pp 11169 (1996).
+  !! Ref: M. Methfessel and A. T. Paxton,, Phys. Rev. B vol 40, pp 3616 (1989).
+  !! Ref: F. Wagner, Th. Laloyaux and M. Scheffler, Phys. Rev. B, vol 57 pp 2102 (1998).
   subroutine electronFill(Eband, filling, TS, E0, Ef, eigenvals, kT, distrib, kWeights)
 
     !> Band structure energy at T
@@ -386,31 +315,27 @@ contains
     real(dp), intent(in) :: kT
 
     !> Choice of distribution functions, currently Fermi, Gaussian and Methfessle-Paxton
-    !> supported. The flags is defined symbolically, so (Methfessel + 2) gives the 2nd order M-P
-    !> scheme
+    !! supported. The flags are defined symbolically, so (Methfessel + 2) gives the 2nd order M-P
+    !! scheme
     integer, intent(in) :: distrib
 
     !> The k-point weightings
     real(dp), intent(in) :: kWeights(:)
 
-    integer :: MPorder
-    integer :: kpts
-    real(dp) :: w
-    real(dp), allocatable :: A(:)
-    real(dp), allocatable :: hermites(:)
-    integer :: i, j , k, l, iSpin
-    real(dp) :: occ, x
+    integer :: ii, iSpin, jj , kk, ll, MPorder, nKpts
+    real(dp), allocatable :: A(:), hermites(:)
+    real(dp) :: beta, compBND, compTS, occ, x
 
     @:ASSERT(size(filling, dim=3) == size(Eband))
     @:ASSERT(size(filling, dim=3) == size(TS))
     @:ASSERT(size(filling, dim=3) == size(E0))
 
-    kpts = size(kWeights)
+    nKpts = size(kWeights)
 
+    beta = 1.0_dp / kT
     Eband(:) = 0.0_dp
     TS(:) = 0.0_dp
     filling(:,:,:) = 0.0_dp
-    w = 1.0_dp / kT
     E0(:) = 0.0_dp
 
     ! The Gaussian and Methfessel-Paxton broadening functions first
@@ -419,85 +344,117 @@ contains
       allocate(A(0:MPorder))
       allocate(hermites(0 : 2 * MPorder))
       call Aweights(A, MPorder)
+
       do iSpin = 1, size(eigenvals,dim=3)
-        do i = 1, kpts
-          do j = 1, size(eigenvals,dim=1)
-            if (eigenvals(j,i,iSpin)>(Ef-3.0_dp*w)) then
-              exit
+
+        compBND = 0.0_dp
+        compTS = 0.0_dp
+        do ii = 1, nKpts
+
+          lpFilled: do jj = 1, size(eigenvals, dim=1)
+            ! Should find with bisection
+            if (eigenvals(jj, ii, iSpin) > Ef - 3.0_dp * beta) then
+              exit lpFilled
             else
-              filling(j,i,iSpin)=1.0_dp
-              Eband(iSpin) = Eband(iSpin) + eigenvals(j,i,iSpin)
+              filling(jj, ii, iSpin) = 1.0_dp
+              call kahan(Eband(iSpin), eigenvals(jj, ii, iSpin), compBND)
             end if
-          end do
-          do k = j, size(eigenvals,dim=1)
-            if (eigenvals(k, i, iSpin) > (Ef + 3.0_dp * w)) then
-              exit
+          end do lpFilled
+
+          lpPartially: do kk = jj, size(eigenvals, dim=1)
+
+            ! Should find with bisection
+            if (eigenvals(kk, ii, iSpin) > Ef + 3.0_dp * beta) then
+              exit lpPartially
             end if
-            x = (eigenvals(k,i,iSpin) - Ef) / kT
+            x = (eigenvals(kk, ii, iSpin) - Ef) * beta
             call hX(hermites, MPorder * 2, x)
+
             ! Gauusian broadened occupancy
             occ = 0.5_dp * erfcwrap(x)
+
             ! Gaussian broadening entropy
-            TS(iSpin) = TS(iSpin) + kWeights(i) * 0.5_dp * exp(-x**2) / sqrt(pi)
+            call kahan(TS(iSpin), kWeights(ii) * 0.5_dp * exp(-x**2) / sqrt(pi), compTS)
+
             ! Methfessel-Paxton occupation sum
-            do l = 1, MPorder
-              occ = occ + A(l) * hermites(2*l-1) * exp(-x**2)
+            do ll = 1, MPorder
+              occ = occ + A(ll) * hermites(2*ll - 1) * exp(-x**2)
             end do
-            filling(k, i, iSpin) = occ
-            ! Sum up the band-structure energy, including k-point weight where needed
-            Eband(iSpin) = Eband(iSpin)&
-                & + kWeights(i) * filling(k, i, iSpin) * eigenvals(k, i, iSpin)
+            filling(kk, ii, iSpin) = occ
+
+            ! Accumulate the band-structure energy
+            call kahan(Eband(iSpin), kWeights(ii) * filling(kk, ii, iSpin)&
+                & * eigenvals(kk, ii, iSpin), compBND)
+
             ! Methfessel-Paxton broadening entropy
-            do l = 1, MPorder
-              TS(iSpin) = TS(iSpin) &
-                  & + kWeights(i) * 0.5_dp * A(l) * hermites(2 * l) * exp(-x**2)
+            do ll = 1, MPorder
+              call kahan(TS(iSpin), kWeights(ii) * 0.5_dp * A(ll) * hermites(2 * ll) * exp(-x**2),&
+                  & compTS)
             end do
-          end do
+
+          end do lpPartially
+
         end do
+
+        Eband(iSpin) = Eband(iSpin) + compBND
+        TS(iSpin) = TS(iSpin) + compTS
+
       end do
-      TS = TS * kT
-      E0(:) = (real(MPorder + 1,dp) * (Eband - TS) + Eband) / real(MPorder + 2, dp)
+
+      TS(:) = TS * kT
+      E0(:) = (real(MPorder + 1, dp) * (Eband - TS) + Eband) / real(MPorder + 2, dp)
+
     else
+
+      ! Fermi function
       do iSpin = 1, size(eigenvals, dim=3)
-        do i = 1, kpts
-          do j = 1, size(eigenvals, dim=1)
-            x = (eigenvals(j, i, iSpin) - Ef) / kT
+
+        compBND = 0.0_dp
+        compTS = 0.0_dp
+
+        do ii = 1, nKpts
+          lpLevels: do jj = 1, size(eigenvals, dim=1)
+            x = (eigenvals(jj, ii, iSpin) - Ef) * beta
             ! Where the compiler does not handle inf gracefully, trap the exponential function for
             ! small values
           #:if EXP_TRAP
             if (x > mExpArg) then
-              filling(j, i, iSpin) = 0.0_dp
+              filling(jj, ii, iSpin) = 0.0_dp
             else
-              filling(j, i, iSpin) = 1.0_dp / (1.0_dp + exp(x))
+              filling(jj, ii, iSpin) = 1.0_dp / (1.0_dp + exp(x))
             endif
           #:else
-            filling(j, i, iSpin) = 1.0_dp / (1.0_dp + exp(x))
+            filling(jj, ii, iSpin) = 1.0_dp / (1.0_dp + exp(x))
           #:endif
-            if (filling(j, i, iSpin) <= elecTol) then
-              exit
-            end if
-            if (filling(j, i, iSpin) > epsilon(0.0_dp) .and.&
-                & filling(j, i, iSpin) < (1.0_dp - epsilon(1.0_dp))) then
+            if (filling(jj, ii, iSpin) <= elecTol) exit lpLevels
+            if (filling(jj, ii, iSpin) > epsilon(0.0_dp) .and.&
+                & filling(jj, ii, iSpin) < (1.0_dp - epsilon(1.0_dp))) then
               ! Fermi-Dirac entropy :
-              TS(iSpin) = TS(iSpin)&
-                  & - kWeights(i) * (filling(j, i, iSpin)* log(filling(j, i, iSpin))&
-                  & + (1.0_dp - filling(j, i, iSpin)) * log(1.0_dp - filling(j, i, iSpin)))
+              call kahan(TS(iSpin),&
+                  & - kWeights(ii) * (filling(jj, ii, iSpin) * log(filling(jj, ii, iSpin))&
+                  & + (1.0_dp - filling(jj, ii, iSpin)) * log(1.0_dp - filling(jj, ii, iSpin))),&
+                  & compTS)
             end if
-            Eband(iSpin) = Eband(iSpin) &
-                & + kWeights(i) * (filling(j, i, iSpin) * eigenvals(j, i, iSpin))
-          end do
+            call kahan(Eband(iSpin), kWeights(ii) * (filling(jj, ii, iSpin)&
+                & * eigenvals(jj, ii, iSpin)), compBND)
+          end do lpLevels
         end do
+
+        Eband(iSpin) = Eband(iSpin) + compBND
+        TS(iSpin) = TS(iSpin) + compTS
+
       end do
       TS(:) = TS * kT
       E0(:) = Eband - 0.5_dp * TS
+
     end if
 
   end subroutine electronFill
 
 
   !> Calculate the weighting factors for the Methfessel-Paxton smearing scheme
-  !>
-  !> Ref: M. Methfessel and A. T. Paxton, Phys. Rev. B Vol 40, pp 3616 (1989)
+  !!
+  !! Ref: M. Methfessel and A. T. Paxton, Phys. Rev. B Vol 40, pp 3616 (1989)
   subroutine Aweights(A,n)
 
     !> Returned weighting values for the scheme
@@ -507,14 +464,16 @@ contains
     integer, intent(in) :: n
 
     real(dp) :: nbang(0:n)
-    integer i
-    @:ASSERT(n>=0)
-    @:ASSERT(size(A)>=n)
+    integer ii
+
+    @:ASSERT(n >= 0)
+    @:ASSERT(size(A) >= n)
     A(:) = 0.0_dp
-    call fact(nbang,n)
-    do i = 0, n
-      A(i) = real((-1)**i,dp)/(nbang(i)*real(4**i,dp)*sqrt(pi))
+    call fact(nbang, n)
+    do ii = 0, n
+      A(ii) = real((-1)**ii, dp) / (nbang(ii) * real(4**ii, dp) * sqrt(pi))
     end do
+
   end subroutine Aweights
 
 
